753a67ca84d065dc12939a401a79c45d70e4846c
clintwilson <clint@wilsonovi.com>
2016-07-06 18:17:23 -0600

    Added contributing license header (after switching to my GH acc instead of my wife's :/)
    Added the License Header from the \CONTRIBUTING document to
    cert\openssl.cnf
    Added the License Header from the \CONTRIBUTING document to
    cert\PowerShell_CACreationScript.ps1

--- cert/PowerShell_CACreationScript.ps1
+++ cert/PowerShell_CACreationScript.ps1
@@ -1,4 +1,21 @@
-﻿#Get to OpenSSL directory
+﻿#=============
+#Copyright 2016 SAS Project Authors. All Rights Reserved.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#=============
+
+
+#Get to OpenSSL directory
 $openssl = Read-Host -Prompt "Please enter the full directory path to your OpenSSL Bin folder. This folder should also have the openssl.cnf file present in it."
 $cnf = Read-Host -Prompt "Enter the name of your configuration file (should be openssl.cnf)"
 cd $openssl
