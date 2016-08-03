932ad7140fc7612ea83055d72e4b84cb3eaf8cec
Clint Wilson <clint@wilsonovi.com>
2016-08-02 23:04:18 -0600

    Update how "ROLE" OIDs are encoded
    This commit primarily:
    1) Changes the "ROLE" OIDs
    Instead of storing the role value as an arbitrary extension with text
    values for each role, this commit stores them in the Certificate
    Policies Extension.
    In order to do that effectively, I broke out the currently 6 possible
    roles into a sub-OID structure of the previous ROLE OID
    (1.3.6.1.4.1.46609.1.1.1-1.3.6.1.4.1.46609.1.1.6)
    2) Adds the OpenSSL configuration and script sections necessary to
    create the CBSD OEM intermediate and EE cert.
    
    I also cleaned up a lot of the variables in the PowerShell script and
    OpenSSL conf file.

--- cert/CACreationScript.sh
+++ cert/CACreationScript.sh
@@ -34,7 +34,7 @@ mkdir -p $CSR
 mkdir -p $KEY
 touch $INDEX
 
-ROOT=wif_root_ca
+ROOT=root_ca
 SIG=_sign
 EC=ecc
 ROOT_VALIDITY=7300
@@ -55,10 +55,10 @@ openssl req -new -x509 -newkey rsa:$ROOT_RSA_KEY_SIZE -sha384 -nodes -days $ROOT
 #EC 2
 #openssl ecparam -out wifECRoot.pkey -name secp384r1 -genkey
 #openssl req -new -key wifECRoot.pkey -sha384 -nodes -days $ROOT_VALIDITY -extensions $ROOT -out wifECRoot.csr -config openssl.cnf
-#openssl ca -create_serial -out wifECRoot.crt -days $ROOT_VALIDITY -policy policy_anything -md sha384 -keyfile wifECRoot.pkey -selfsign -extensions wif_root_ca -config openssl.cnf -infiles wifECRoot.csr
+#openssl ca -create_serial -out wifECRoot.crt -days $ROOT_VALIDITY -policy policy_anything -md sha384 -keyfile wifECRoot.pkey -selfsign -extensions root_ca -config openssl.cnf -infiles wifECRoot.csr
 
 #SAS Intermediate
-SAS=wif_sas_ca
+SAS=sas_ca
 SAS_SIGN=$SAS$SIG
 
 openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $SAS -out $SAS.csr -keyout $SAS.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA SAS CA-1" -config openssl.cnf
@@ -66,7 +66,7 @@ openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $SAS -out $SAS.cs
 openssl ca -create_serial -cert $ROOT.crt -keyfile $ROOT.pkey -outdir ./root/ca/newcerts -batch -out $SAS.crt -utf8 -days $INT_VALIDITY -policy policy_anything -md sha384 -extensions $SAS_SIGN -config openssl.cnf -in $SAS.csr
 
 #SAS EE
-SAS_EE=wif_sas_req
+SAS_EE=sas_req
 SAS_EE_SIGN=$SAS_EE$SIG
 
 openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $SAS_EE -out $SAS_EE.csr -keyout $SAS_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=SAS End-Entity Example" -config openssl.cnf
@@ -74,7 +74,7 @@ openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $SAS_EE -out $SAS_
 openssl ca -create_serial -cert $SAS.crt -keyfile $SAS.pkey -outdir $CSR -batch -out $SAS_EE.crt -utf8 -days $EE_VALIDITY -policy policy_anything -md sha384 -extensions $SAS_EE_SIGN -config openssl.cnf -in $SAS_EE.csr
 
 #Operator Intermediate
-OPER=wif_oper_ca
+OPER=oper_ca
 OPER_SIGN=$OPER$SIG
 
 openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $OPER -out $OPER.csr -keyout $OPER.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA Operator CA-1" -config openssl.cnf
@@ -82,7 +82,7 @@ openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $OPER -out $OPER.
 openssl ca -create_serial -cert $ROOT.crt -keyfile $ROOT.pkey -outdir $CSR -batch -out $OPER.crt -utf8 -days $INT_VALIDITY -policy policy_anything -md sha384 -extensions $OPER_SIGN -config openssl.cnf -in $OPER.csr
 
 #Operator EE
-OPER_EE=wif_oper_req
+OPER_EE=oper_req
 OPER_EE_SIGN=$OPER_EE$SIG
 
 openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $OPER_EE -out $OPER_EE.csr -keyout $OPER_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=Operator End-Entity Example" -config openssl.cnf
@@ -90,7 +90,7 @@ openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $OPER_EE -out $OPE
 openssl ca -create_serial -cert $OPER.crt -keyfile $OPER.pkey -outdir $CSR -batch -out $OPER_EE.crt -utf8 -days $EE_VALIDITY -policy policy_anything -md sha384 -extensions $OPER_EE_SIGN -config openssl.cnf -in $OPER_EE.csr
 
 #Professional Installer Intermediate
-PI=wif_pi_ca
+PI=pi_ca
 PI_SIGN=$PI$SIG
 
 openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $PI -out $PI.csr -keyout $PI.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA Installer CA-1" -config openssl.cnf
@@ -98,7 +98,7 @@ openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $PI -out $PI.csr 
 openssl ca -create_serial -cert $ROOT.crt -keyfile $ROOT.pkey -outdir $CSR -batch -out $PI.crt -utf8 -days $INT_VALIDITY -policy policy_anything -md sha384 -extensions $PI_SIGN -config openssl.cnf -in $PI.csr
 
 #Professional Installer EE
-PI_EE=wif_pi_req
+PI_EE=pi_req
 PI_EE_SIGN=$PI_EE$SIG
 
 openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $PI_EE -out $PI_EE.csr -keyout $PI_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=Installer End-Entity Example" -config openssl.cnf
@@ -108,25 +108,33 @@ openssl ca -create_serial -cert $PI.crt -keyfile $PI.pkey -outdir $CSR -batch -o
 #PAL: TODO
 
 #Device Intermediate
-CBSD=wif_cbsd_ca
+CBSD=cbsd_ca
 CBSD_SIGN=$CBSD$SIG
 
-openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $CBSD -out $CBSD.csr -keyout $CBSD.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA Device CA-1" -config openssl.cnf
+openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $CBSD -out $CBSD.csr -keyout $CBSD.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA CBSD CA-1" -config openssl.cnf
 
 openssl ca -create_serial -cert $ROOT.crt -keyfile $ROOT.pkey -outdir $CSR -batch -out $CBSD.crt -utf8 -days $INT_VALIDITY -policy policy_anything -md sha384 -extensions $CBSD_SIGN -config openssl.cnf -in $CBSD.csr
 
 #Device EE
-CBSD_EE=wif_cbsd_req
+CBSD_EE=cbsd_req
 CBSD_EE_SIGN=$CBSD_EE$SIG
 
-openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $CBSD_EE -out $CBSD_EE.csr -keyout $CBSD_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=Device End-Entity Example" -config openssl.cnf
+openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $CBSD_EE -out $CBSD_EE.csr -keyout $CBSD_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=CBSD End-Entity Example" -config openssl.cnf
 
 openssl ca -create_serial -cert $CBSD.crt -keyfile $CBSD.pkey -outdir $CSR -batch -out $CBSD_EE.crt -utf8 -days $EE_VALIDITY -policy policy_anything -md sha384 -extensions $CBSD_EE_SIGN -config openssl.cnf -in $CBSD_EE.csr
 
-#TODO: manufacturer intermediate signed by $CBSD, and EE cert signed by manufacturer intermediate
+#Device OEM Intermediate
+CBSD_OEM=cbsd_oem_ca
+CBSD_OEM_SIGN=$CBSD_OEM$SIG
 
+openssl req -new -newkey rsa:$INT_RSA_KEY_SIZE -nodes -reqexts $CBSD_OEM -out $CBSD_OEM.csr -keyout $CBSD_OEM.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=WInnForum RSA CBSD OEM CA-1" -config openssl.cnf
 
+openssl ca -create_serial -cert $CBSD.crt -keyfile $CBSD.pkey -outdir $CSR -batch -out $CBSD_OEM.crt -utf8 -days $INT_VALIDITY -policy policy_anything -md sha384 -extensions $CBSD_OEM_SIGN -config openssl.cnf -in $CBSD_OEM.csr
 
+#Device OEM EE
+CBSD_OEM_EE=cbsd_oem_req
+CBSD_OEM_EE_SIGN=$CBSD_OEM_EE$SIG
 
+openssl req -new -newkey rsa:$EE_RSA_KEY_SIZE -nodes -reqexts $CBSD_OEM_EE -out $CBSD_OEM_EE.csr -keyout $CBSD_OEM_EE.pkey -subj "/C=US/ST=District of Columbia/L=Washington/O=Wireless Innovation Forum/OU=www.wirelessinnovation.org/CN=CBSD OEM End-Entity Example" -config openssl.cnf
 
-
+openssl ca -create_serial -cert $CBSD_OEM.crt -keyfile $CBSD_OEM.pkey -outdir $CSR -batch -out $CBSD_OEM_EE.crt -utf8 -days $EE_VALIDITY -policy policy_anything -md sha384 -extensions $CBSD_OEM_EE_SIGN -config openssl.cnf -in $CBSD_OEM_EE.csr
