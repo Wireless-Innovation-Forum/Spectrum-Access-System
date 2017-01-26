b2d4e171c96408386d10dca116642b28b211ea6e
liji94188 <liji94188@gmail.com>
2017-01-25 21:50:43 -0800

    Update registration_testcase.py

--- src/harness/testcases/registration_testcase.py
+++ src/harness/testcases/registration_testcase.py
@@ -28,6 +28,11 @@ class RegistrationTestcase(unittest.TestCase):
     pass
 
   def test_10_3_4_1_1_1(self):
+    """New Multi-Step registration for CBSD Cat A (No existing CBSD ID).
+
+    The response should be SUCCESS.
+    """
+
     # Register the device
     device_a = json.load(
         open(os.path.join('testcases', 'testdata', 'device_a.json')))
@@ -35,3 +40,53 @@ class RegistrationTestcase(unittest.TestCase):
     response = self._sas.Registration(request)['registrationResponse'][0]
     # Check registration response
     self.assertEqual(response['response']['responseCode'], 0)
+
+  def test_10_3_4_2_1(self):
+    """CBSD registration request with missing required parameter.
+
+    The required parameter 'userId' is missing in a registration request,
+    the response should be FAIL.
+    """
+
+    # Register the device, make sure at least one required parameter is missing
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    del device_a['userId']
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 102)
+
+  def test_10_3_4_2_5_1(self):
+    """CBSD registration request with invalid required parameter.
+
+    The value of required parameter 'fccId' is invalid(exceeds its max
+    length) in the registration request, the response should be FAIL.
+    """
+
+    # Register the device, make sure at least one required parameter is invalid
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    device_a['fccId'] = 'abcdefghijklmnopqrstuvwxyz'
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertFalse('cbsdId' in response)
+    self.assertEqual(response['response']['responseCode'], 103)
+
+  def test_10_3_4_2_5_3(self):
+    """CBSD registration request with invalid conditional parameter.
+
+    The value of conditional parameter 'radioTechnology' of airInterface
+    object is invalid in the registration request, the response should be FAIL.
+    """
+    # Register the device, make sure at least one conditional parameter is
+    # invalid
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    device_a['airInterface']['radioTechnology'] = 'invalid value'
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertFalse('cbsdId' in response)
+    self.assertEqual(response['response']['responseCode'], 103)
