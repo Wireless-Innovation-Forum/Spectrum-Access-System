f46adf176953f7a413b3f1a0c9894f94ea59cace
Qing Wu <qing.wu@federatedwireless.com>
2017-03-01 14:30:03 -0500

    Add CBSD-SAS relinquishment test 10.13.4.1.2

--- src/harness/testcases/relinquishment_testcase.py
+++ src/harness/testcases/relinquishment_testcase.py
@@ -70,3 +70,55 @@ class RelinquishmentTestcase(unittest.TestCase):
     self.assertEqual(response['cbsdId'], cbsd_id)
     self.assertEqual(response['grantId'], grant_id)
     self.assertEqual(response['response']['responseCode'], 0)
+
+  def test_10_13_4_1_2(self):
+    """Multiple iterative CBSD relinquishments.
+
+    CBSD Harness sends multiple Relinquishment Requests to SAS including CBSD 
+    ID and Grant ID in correct format. The response should be SUCCESS.
+    """
+
+    # Register the device
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    cbsd_id = response['cbsdId']
+    del request, response
+
+    # Request three grants
+    grant_id = []
+    for i in range(0, 3):
+      grant = json.load(
+          open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+      grant['cbsdId'] = cbsd_id
+      grant['operationParam']['operationFrequencyRange']['lowFrequency'] = (
+          3600000000.0 + i * 10000000.0)
+      grant['operationParam']['operationFrequencyRange']['highFrequency'] = (
+          3610000000.0 + i * 10000000.0)
+      request = {'grantRequest': [grant]}
+      # Check grant response
+      response = self._sas.Grant(request)['grantResponse'][0]
+      self.assertEqual(response['cbsdId'], cbsd_id)
+      self.assertTrue(response['grantId'])
+      self.assertEqual(response['response']['responseCode'], 0)
+      grant_id.append(response['grantId'])
+      del request, response
+
+    # Relinquish second grant, then first grant, then third grant
+    for i in [1, 0, 2]:
+      request = {
+          'relinquishmentRequest': [{
+              'cbsdId': cbsd_id,
+              'grantId': grant_id[i]
+          }]
+      }
+      response = self._sas.Relinquishment(request)['relinquishmentResponse'][0]
+      # Check the relinquishment response
+      self.assertEqual(response['cbsdId'], cbsd_id)
+      self.assertEqual(response['grantId'], grant_id[i])
+      self.assertEqual(response['response']['responseCode'], 0)
+      
