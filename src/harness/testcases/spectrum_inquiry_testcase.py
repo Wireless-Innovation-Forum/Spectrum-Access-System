497e9196e2a9f86b4904c0dd9a0e2ce859d5f907
Sunil Kidambi <kidambis@sassy.mtv.corp.google.com>
2017-03-15 16:27:46 -0700

    Implement WINFF.FT.S.SIQ.10
    Missing highFrequency parameter
    Old test case #6.2.4.2.1.2.2

--- src/harness/testcases/spectrum_inquiry_testcase.py
+++ src/harness/testcases/spectrum_inquiry_testcase.py
@@ -1,21 +1,9 @@
-#    Copyright 2016 SAS Project Authors. All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
 import json
 import os
 import unittest
 
 import sas
+from util import winnforum_testcase
 
 
 class SpectrumInquiryTestcase(unittest.TestCase):
@@ -27,16 +15,16 @@ class SpectrumInquiryTestcase(unittest.TestCase):
   def tearDown(self):
     pass
 
-  def test_10_5_4_1_1_3(self):
-    """Include the entire CBRS band in the spectrumInquiryRequest
+  @winnforum_testcase
+  def test_WINNF_FT_S_SIQ_10(self):
+    """Send Spectrum Inquiry with missing highFrequency parameter.
 
-    This test ensures that SAS is able to handle spectrum inquiry of
-    the entire CBRS band.
+    The response should be MISSING_PARAM, code 102
     """
-
     # Register the device
     device_a = json.load(
         open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
     request = {'registrationRequest': [device_a]}
     response = self._sas.Registration(request)['registrationResponse'][0]
     # Check registration response
@@ -44,14 +32,14 @@ class SpectrumInquiryTestcase(unittest.TestCase):
     cbsd_id = response['cbsdId']
     del request, response
 
-    # Request spectrum inquiry
+    # Send Spectrum Inquiry request
     spectrum_inquiry_0 = json.load(
         open(os.path.join('testcases', 'testdata', 'spectrum_inquiry_0.json')))
     spectrum_inquiry_0['cbsdId'] = cbsd_id
-    spectrum_inquiry_0['inquiredSpectrum'][0]['lowFrequency']  = 3550000000.0
-    spectrum_inquiry_0['inquiredSpectrum'][0]['highFrequency'] = 3700000000.0
+    del spectrum_inquiry_0['inquiredSpectrum'][0]['highFrequency']
+    self.assertFalse(
+        'highFrequency' in spectrum_inquiry_0['inquiredSpectrum'][0])
     request = {'spectrumInquiryRequest': [spectrum_inquiry_0]}
+    # Check Spectrum Inquiry Response
     response = self._sas.SpectrumInquiry(request)['spectrumInquiryResponse'][0]
-    # Check spectrum inquiry response
-    self.assertEqual(response['cbsdId'], cbsd_id)
-    self.assertEqual(response['response']['responseCode'], 0)
+    self.assertEqual(response['response']['responseCode'], 102)
