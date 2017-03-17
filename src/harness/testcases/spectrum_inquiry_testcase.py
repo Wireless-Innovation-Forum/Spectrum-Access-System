cc20744ff21bd03190ff864f5dfd168d22309536
Sunil Kidambi <kidambis@sassy.mtv.corp.google.com>
2017-03-17 13:51:15 -0700

    Implement WINFF.FT.S.SIQ.8 Missing cbsdId
    Also named 6.2.4.2.1.1

--- src/harness/testcases/spectrum_inquiry_testcase.py
+++ src/harness/testcases/spectrum_inquiry_testcase.py
@@ -1,17 +1,3 @@
-#    Copyright 2016 SAS Project Authors. All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
 import json
 import os
 import unittest
@@ -28,6 +14,32 @@ class SpectrumInquiryTestcase(unittest.TestCase):
 
   def tearDown(self):
     pass
+
+  @winnforum_testcase
+  def test_WINFF_FT_S_SIQ_8(self):
+    """Send Spectrum Inquiry with missing cbsdId field.
+
+    The response should be MISSING_PARAM, code 102
+    """
+    # Register the device
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    del request, response
+
+    # Send Spectrum Inquiry request
+    spectrum_inquiry_0 = json.load(
+        open(os.path.join('testcases', 'testdata', 'spectrum_inquiry_0.json')))
+    self.assertFalse('cbsdId' in spectrum_inquiry_0)
+    request = {'spectrumInquiryRequest': [spectrum_inquiry_0]}
+    # Check Spectrum Inquiry Response
+    response = self._sas.SpectrumInquiry(request)['spectrumInquiryResponse'][0]
+    self.assertFalse('cbsdId' in response)
+    self.assertEqual(response['response']['responseCode'], 102)
 
   @winnforum_testcase
   def test_WINNF_FT_S_SIQ_10(self):
@@ -51,9 +63,9 @@ class SpectrumInquiryTestcase(unittest.TestCase):
         open(os.path.join('testcases', 'testdata', 'spectrum_inquiry_0.json')))
     spectrum_inquiry_0['cbsdId'] = cbsd_id
     del spectrum_inquiry_0['inquiredSpectrum'][0]['highFrequency']
+    self.assertFalse(
+        'highFrequency' in spectrum_inquiry_0['inquiredSpectrum'][0])
     request = {'spectrumInquiryRequest': [spectrum_inquiry_0]}
     # Check Spectrum Inquiry Response
     response = self._sas.SpectrumInquiry(request)['spectrumInquiryResponse'][0]
-    self.assertTrue('cbsdId' in response)
-    self.assertEqual(response['cbsdId'], cbsd_id)
     self.assertEqual(response['response']['responseCode'], 102)
