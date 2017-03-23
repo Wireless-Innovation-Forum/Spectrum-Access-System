82740db3365a4e82ac6281fd91bbb62c494104fd
Sunil Kidambi <krlinus@yahoo.com>
2017-03-23 10:46:49 -0700

    Implement WINFF.FT.S.SIQ.12
    cbsdId sent in the spectrumInquiry is different
    from its assigned cbsdId, but the cbsdId does not exist in the SAS

--- src/harness/testcases/spectrum_inquiry_testcase.py
+++ src/harness/testcases/spectrum_inquiry_testcase.py
@@ -57,3 +57,30 @@ class SpectrumInquiryTestcase(unittest.TestCase):
     self.assertTrue('cbsdId' in response)
     self.assertEqual(response['cbsdId'], cbsd_id)
     self.assertEqual(response['response']['responseCode'], 102)
+
+  @winnforum_testcase
+  def test_WINFF_FT_S_SIQ_12(self):
+    """Send Spectrum Inquiry with non-existent cbsdId parameter.
+
+    The response should be INVALID_PARAM, code 103
+    """
+    # Register the device
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    cbsd_id = response['cbsdId']
+    del request, response
+
+    # Send Spectrum Inquiry request
+    spectrum_inquiry_0 = json.load(
+        open(os.path.join('testcases', 'testdata', 'spectrum_inquiry_0.json')))
+    spectrum_inquiry_0['cbsdId'] = cbsd_id + '-changed'
+    self.assertNotEqual(cbsd_id, spectrum_inquiry_0['cbsdId'])
+    request = {'spectrumInquiryRequest': [spectrum_inquiry_0]}
+    # Check Spectrum Inquiry Response
+    response = self._sas.SpectrumInquiry(request)['spectrumInquiryResponse'][0]
+    self.assertTrue(response['response']['responseCode'] in (103, 105))
