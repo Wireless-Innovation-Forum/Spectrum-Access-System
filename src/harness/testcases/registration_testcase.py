63a01692868a2f0c25a1be803d0bc7c86946ce42
Janet Liu <janetliuboston@users.noreply.github.com>
2017-03-24 15:20:31 -0400

    add registration test WINFF.FT.S.REG.8 (#122)

--- src/harness/testcases/registration_testcase.py
+++ src/harness/testcases/registration_testcase.py
@@ -108,6 +108,33 @@ class RegistrationTestcase(unittest.TestCase):
     self.assertEqual(response['response']['responseCode'], 0)
 
   @winnforum_testcase
+  def test_WINFF_FT_S_REG_8(self):
+    """ Re-registration of Single-step-registered CBSD (CBSD ID exists)
+
+    The response should be SUCCESS.
+    """
+
+    # Register the device
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    device_a['measCapability'] = []
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertTrue('cbsdId' in response)
+    self.assertEqual(response['response']['responseCode'], 0)
+
+    del response
+
+    # Re-register the device
+    response = self._sas.Registration(request)['registrationResponse'][0]
+
+    self.assertTrue('cbsdId' in response)
+    self.assertFalse('measReportConfig' in response)
+    self.assertEqual(response['response']['responseCode'], 0)
+
+  @winnforum_testcase
   def test_10_3_4_2_1(self):
     """CBSD registration request with missing required parameter.
 
