7f27be7f3c39e7abd9a08681a72b56e3de25e19c
Qing Wu <qing.wu@federatedwireless.com>
2017-03-28 17:28:40 -0400

    add relinquishment test WINFF.FT.S.RLQ.8

--- src/harness/testcases/relinquishment_testcase.py
+++ src/harness/testcases/relinquishment_testcase.py
@@ -229,6 +229,65 @@ class RelinquishmentTestcase(unittest.TestCase):
     self.assertEqual(response['response']['responseCode'], 103)
 
   @winnforum_testcase
+  def test_WINFF_FT_S_RLQ_8(self):
+    """CBSD relinquishment request of grant that is previously
+    relinquished.
+
+    CBSD Harness sends Relinquishment Request to SAS including CBSD ID and
+    Grant ID in correct format but the Grant is already relinquished.
+    The response should be FAIL.
+    """
+
+    # Register the device
+    device = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    #self._sas_admin.InjectFccId({'fccId': device['fccId']})
+    request = {'registrationRequest': [device]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    cbsd_id = response['cbsdId']
+    del request, response
+
+    # Request grant
+    grant = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant['cbsdId'] = cbsd_id
+    request = {'grantRequest': [grant]}
+    # Check grant response
+    response = self._sas.Grant(request)['grantResponse'][0]
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    self.assertEqual(response['response']['responseCode'], 0)
+    grant_id = response['grantId']
+    del request, response
+
+    # Relinquish the grant
+    request = {
+        'relinquishmentRequest': [{
+            'cbsdId': cbsd_id,
+            'grantId': grant_id
+        }]
+    }
+    response = self._sas.Relinquishment(request)['relinquishmentResponse'][0]
+    # Check the relinquishment response
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    self.assertEqual(response['grantId'], grant_id)
+    self.assertEqual(response['response']['responseCode'], 0)
+
+    # Relinquish the grant again
+    request = {
+        'relinquishmentRequest': [{
+            'cbsdId': cbsd_id,
+            'grantId': grant_id
+        }]
+    }
+    response = self._sas.Relinquishment(request)['relinquishmentResponse'][0]
+    # Check the relinquishment response
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    self.assertFalse('grantId' in response)
+    self.assertEqual(response['response']['responseCode'], 103)
+
+  @winnforum_testcase
   def test_WINFF_FT_S_RLQ_12(self):
     """CBSD relinquishment request with missing CBSD ID
     relinquished.
