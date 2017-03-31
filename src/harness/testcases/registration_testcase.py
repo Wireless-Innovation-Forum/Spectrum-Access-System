ab038365bd46d3d27a5aca2b2392e0ab0f7ab294
Janet Liu <janetliuboston@users.noreply.github.com>
2017-03-31 15:30:21 -0400

    add registration test WINNF.FT.S.REG.22 (#157)

--- src/harness/testcases/registration_testcase.py
+++ src/harness/testcases/registration_testcase.py
@@ -732,6 +732,44 @@ class RegistrationTestcase(unittest.TestCase):
     self.assertFalse('cbsdId' in response)
 
   @winnforum_testcase
+  def test_WINNF_FT_S_REG_22(self):
+    """Group Error in Array request (responseCode 201)
+    The response should be SUCCESS for the first two devices,
+    FAILURE for the third device.
+    """
+
+    # Register the devices
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    device_b = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_b.json')))
+    device_c = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_c.json')))
+
+    # Device #3 invalid group - only 'INTERFERENCE_COORDINATION' allowed
+    device_c['groupingParam'] = [
+        {'groupType': 'FAKE_GROUP_TYPE',
+         'groupId': '1234'}
+    ]
+
+    # Inject FCC IDs
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    self._sas_admin.InjectFccId({'fccId': device_b['fccId']})
+    self._sas_admin.InjectFccId({'fccId': device_c['fccId']})
+
+    # Register devices
+    devices = [device_a, device_b, device_c]
+    request = {'registrationRequest': devices}
+    response = self._sas.Registration(request)
+
+    # Check response
+    for resp in response['registrationResponse'][:2]:
+        self.assertEqual(resp['response']['responseCode'], 0)
+        self.assertTrue('cbsdId' in resp)
+    self.assertTrue(response['registrationResponse'][2]['response']['responseCode'] in (103, 201))
+    self.assertFalse('cbsdId' in response['registrationResponse'][2])
+
+  @winnforum_testcase
   def test_WINNF_FT_S_REG_26(self):
     """Category Error in Array request (responseCode 202)
 
