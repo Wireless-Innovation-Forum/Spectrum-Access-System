4a96d8db5dd3899218c6b24f916d1a88491d60ce
Sushant Parkhi <sushantparkhi@gmail.com>
2017-05-05 15:47:44 -0400

    Added Return keyword in InjectEscZone

--- src/harness/sas.py
+++ src/harness/sas.py
@@ -165,7 +165,7 @@ class SasAdminImpl(sas_interface.SasAdminInterface):
                  self._GetDefaultAdminSSLKeyPath())
 
   def InjectEscZone(self, request):
-    _RequestPost('https://%s/admin/injectdata/esc_zone' % self._base_url, request,
+    return _RequestPost('https://%s/admin/injectdata/esc_zone' % self._base_url, request,
                  self._GetDefaultAdminSSLCertPath(),
                  self._GetDefaultAdminSSLKeyPath())
 
