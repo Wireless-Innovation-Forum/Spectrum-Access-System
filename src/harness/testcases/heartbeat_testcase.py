9eee749519d38f0a62672975903227886431c831
Sushant Deepak Parkhi <sushant@bu.edu>
2017-05-10 17:23:41 -0400

    Added WINNF.FT.S.HBT.17 (#185)
    * Added WINNF_FT_S_HBT_17
    
    * Changed the Order of the Heartbeat Request Helper Method
    
    * Fixed Issues for HBT.17
    
    * Added 1 Second to Time Difference
    
    * Fixed Issues in Assertion and Time Difference

--- src/harness/testcases/heartbeat_testcase.py
+++ src/harness/testcases/heartbeat_testcase.py
@@ -12,10 +12,11 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 from datetime import datetime
+import time
 import json
 import os
 import unittest
-
+import logging
 import sas
 from util import winnforum_testcase
 
@@ -741,6 +742,58 @@ class HeartbeatTestcase(unittest.TestCase):
     self.assertLessEqual(
         datetime.strptime(response['transmitExpireTime'], '%Y-%m-%dT%H:%M:%SZ'),
         transmit_expire_time_1)
+
+  @winnforum_testcase
+  def test_WINNF_FT_S_HBT_17(self):
+    """Heartbeat Request from CBSD in Registered state (immediately after CBSD's grant is expired)
+
+    Response Code should  be 103 or 500"""
+
+    # Register the device
+    device_a = json.load(
+      open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
+    request = {'registrationRequest': [device_a]}
+    response = self._sas.Registration(request)['registrationResponse'][0]
+
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    cbsd_id = response['cbsdId']
+    del request, response
+
+    # Request grant
+    grant_0 = json.load(open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_0['cbsdId'] = cbsd_id
+    request = {'grantRequest': [grant_0]}
+
+    # Check grant response
+    response = self._sas.Grant(request)['grantResponse'][0]
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    self.assertEqual(response['response']['responseCode'], 0)
+    grant_id = response['grantId']
+    grant_expire_time = datetime.strptime(response['grantExpireTime'], '%Y-%m-%dT%H:%M:%SZ')
+    del request, response
+
+    # Calculate the Difference Between Current Time and the GrantExpireTime
+    difference_time = (grant_expire_time - datetime.utcnow()).total_seconds()
+    logging.debug('Difference between grantExpireTime and CurrentTime (in seconds) ', difference_time)
+    self.assertGreaterEqual(grant_expire_time, datetime.utcnow())
+    time.sleep(difference_time + 1)
+
+    # Request Heartbeat
+    request = {
+      'heartbeatRequest': [{
+        'cbsdId': cbsd_id,
+        'grantId': grant_id,
+        'operationState': 'GRANTED'
+      }]
+    }
+
+    response = self._sas.Heartbeat(request)['heartbeatResponse'][0]
+    # Check the heartbeat response
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    # Response Should fail with Code 103 or 500
+    self.assertTrue(response['response']['responseCode'] in (103, 500))
 
   @winnforum_testcase
   def test_WINNF_FT_S_HBT_18(self):
