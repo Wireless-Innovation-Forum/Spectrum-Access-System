e6783244ece9e8761b548114b3b95614c1678a2d
Sunil <krlinus@yahoo.com>
2017-06-01 10:32:09 -0700

    Update heartbeat_testcase.py
    Correct erroneous merge. TBD: re-test.

--- src/harness/testcases/heartbeat_testcase.py
+++ src/harness/testcases/heartbeat_testcase.py
@@ -950,16 +950,28 @@ class HeartbeatTestcase(unittest.TestCase):
     Heartbeat request immediately after CBSD moves out of Granted State. The
     grantId is invalid in heartbeat request. The response should be FAIL.
     """
+
     # Register the device
     device_a = json.load(
         open(os.path.join('testcases', 'testdata', 'device_a.json')))
     self._sas_admin.InjectFccId({'fccId': device_a['fccId']})
     request = {'registrationRequest': [device_a]}
     response = self._sas.Registration(request)['registrationResponse'][0]
+    # Check registration response
+    self.assertEqual(response['response']['responseCode'], 0)
+    cbsd_id = response['cbsdId']
+    del request, response
+
+    # Request grant    
     grant_0 = json.load(
         open(os.path.join('testcases', 'testdata', 'grant_0.json')))
     grant_0['cbsdId'] = cbsd_id
     request = {'grantRequest': [grant_0]}
+    # Check grant response
+    response = self._sas.Grant(request)['grantResponse'][0]
+    self.assertEqual(response['cbsdId'], cbsd_id)
+    self.assertEqual(response['response']['responseCode'], 0)
+    grant_id = response['grantId']
     grant_expire_time = datetime.strptime(response['grantExpireTime'],
                                           '%Y-%m-%dT%H:%M:%SZ')
     del request, response
