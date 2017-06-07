86ff4fa27a4ac484317fc303149d029b148aa4c6
Sushant Parkhi <sushantparkhi@gmail.com>
2017-06-07 17:56:48 -0400

    Fixed issue in sas_interface

--- src/harness/sas_testcase.py
+++ src/harness/sas_testcase.py
@@ -32,18 +32,6 @@ class SasTestCase(sas_interface.SasTestcaseInterface, unittest.TestCase):
     validate(response, schema, resolver=resolver)
 
   def assertValidResponseFormatForApprovedGrant(self, grant_response):
-    """Validate an approved grant response.
-
-    Check presence and basic validity of each required field.
-    Check basic validity of optional fields if they exist.
-    Args:
-      grant_response: A dictionary with a single grant response object from an
-        array originally returned by a SAS server as specified in TS
-
-    Returns:
-      Nothing. It asserts if something about the response is broken/not per
-      specs. Assumes it is dealing with an approved request.
-    """
     # Check required string fields
     for field_name in ('cbsdId', 'grantId', 'grantExpireTime', 'channelType'):
       self.assertTrue(field_name in grant_response)
