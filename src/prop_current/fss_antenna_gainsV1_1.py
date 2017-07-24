64852469dc6722fe9f15f0b0bdf00a7c549b3421
gerard garnier <ggarnier>
2017-07-24 14:05:32 +0200

    Fix bug in reference implementation for FSS_antenna_gain computation

--- src/prop_current/fss_antenna_gainsV1_1.py
+++ src/prop_current/fss_antenna_gainsV1_1.py
@@ -5,58 +5,54 @@ Created on 6 juil. 2017
 Gerard Garnier
 '''
 import vincenty
-from ehata_its_wf import *
-from cmath import isnan
+import numpy as npy
+import math
 
-def GetFSSAntennaGain(pfl, h_b__meter, h_m__meter, antennaLat, antennaLon, FSSLat, FSSLon, antennaElevation, alpha, w1, w2):
+def GetFSSAntennaGain(pfl, cbsdLat, cbsdLng, cbsdHeight, fssLat, fssLng, fssHeight, fssPointingElevation, fssPointingAzimuth, w1=None, w2=None):
     """
     Returns the FSS Earth Station Antenna Gain (R2-SGN-21).
     Inputs:
         pfl                       Terrain profile line
-        h_b__meter                height of the CBSD, in meters
-        h_m__meter                height of the FSS, in meters
-        antennaElevation          the FSS earth station antenna elevation (deg : 0 - 359)        
-        alpha                     FSS earth station antenna azimuth (deg : 0 - 359)
+        cbsdLat, cbsdLng          lat/lon of cbsd (deg)
+        cbsdHeight                height of the CBSD, in meters
+        fssLat, fssLng            lat/lon of fss (deg) 
+        fssHeight                 height of the FSS, in meters
+        fssPointingElevation      the FSS earth station antenna elevation (deg : 0 - 359)        
+        fssPointingAzimuth        FSS earth station antenna azimuth (deg : 0 - 359)
         w1, w2                    weights 
     Output:
         FSS Earth Station Antenna Gain
     """
-    d, azstart, AZ = vincenty.dist_bear_vincenty(antennaLat, antennaLon, FSSLat, FSSLon)
+    d, azstart, AZ = vincenty.dist_bear_vincenty(cbsdLat, cbsdLng, fssLat, fssLng)
 
     theta = [0., 0.]
-    GetElevationAngle(pfl, h_m__meter, h_b__meter, theta)
+    GetElevationAngle(pfl, fssHeight, cbsdHeight, theta)
     
-    theta[0] = npy.degrees(theta[0])
-    if(theta[0] < 0) :
-        theta[0]+=360
             
-    """
-    off-axis angle
-    """
-    ThetaAngle = (180 * (1 / math.cos( (math.cos(theta[0]) * math.cos(antennaElevation)\
-                                        * math.cos(alpha - AZ)) + (math.sin(theta[0]) * math.sin(antennaElevation)) )))/ math.pi
+    # off-axis angle
+    ThetaAngle = (180 * (1 / math.cos( (math.cos(theta[0]) * math.cos(math.radians(fssPointingElevation))\
+                                        * math.cos(math.radians(fssPointingAzimuth - AZ))) + (math.sin(theta[0]) * math.sin(math.radians(fssPointingElevation))) )))/ math.pi
         
-    Ggso, GgsoT = GetGgsoAndGgsoT(ThetaAngle)
+    Ggso, GgsoP = GetGgsoAndGgsoP(ThetaAngle)
     
-    """
-    If the FSS earth station registration data includes values for w1 and w2, 
-    SAS shall use these values. Otherwise SAS shall assume w1=0 and w2=1.
-    """
-    if isnan(w1) or isnan(w2):
-        FSSGain = GgsoT
+    # If the FSS earth station registration data includes values for w1 and w2, 
+    # SAS shall use these values. Otherwise SAS shall assume w1=0 and w2=1.
+    if w1 is None or w2 is None:
+        FSSGain = GgsoP
     else:
-        FSSGain = w1*Ggso + w2*GgsoT
+        FSSGain = w1*Ggso + w2*GgsoP
     
     return FSSGain
 
-def GetElevationAngle(pfl, h_m__meter, h_b__meter, theta):
+def GetElevationAngle(pfl, fssHeight, cbsdHeight, theta):
     
     """
     Returns elevation angles of the horizons from each terminal.
+    extract and rewrite of the ITM horizon calculation routine hzns()
     Inputs:
         pfl          Terrain profile line
-        h_m__meter   height of the mobile, in meters
-        h_b__meter   height of the base station, in meters
+        fssHeight   height of the mobile, in meters
+        cbsdHeight   height of the base station, in meters
     Output:
         theta : Elevation angles
                  - theta[0] = mobile horizon distance, in radians
@@ -66,11 +62,13 @@ def GetElevationAngle(pfl, h_m__meter, h_b__meter, theta):
     xi = pfl[1]                #// step size of the profile points, in meter
     d_meter = np * xi
 
-    h_gnd__meter = AverageTerrainHeight(pfl)
+    h_gnd__meter = npy.mean(pfl[2:])
     
+    # Effective earth curvature per meter, based on refractivity ens 
+    # at average altitude based on refractivity en0 at sea level
     en0 = 301.0
     ens = 0
-    if (h_gnd__meter == 0):
+    if h_gnd__meter == 0:
         ens = en0
     else:
         ens = en0 * math.exp(-h_gnd__meter / 9460)
@@ -78,8 +76,8 @@ def GetElevationAngle(pfl, h_m__meter, h_b__meter, theta):
 
     gme = gma * (1 - 0.04665 * math.exp(ens / 179.3))
    
-    za = pfl[2] + h_m__meter
-    zb = pfl[np + 2] + h_b__meter
+    za = pfl[2] + fssHeight
+    zb = pfl[np + 2] + cbsdHeight
     qc = 0.5 * gme
     q = qc * d_meter
     theta[1] = (zb - za) / d_meter
@@ -106,32 +104,35 @@ def GetElevationAngle(pfl, h_m__meter, h_b__meter, theta):
                 theta[1] = theta[1] + q / sb
 
     
-def GetGgsoAndGgsoT(Theta):
+def GetGgsoAndGgsoP(Theta):
     """
     Returns GGSO Patterns.
     Inputs:
         Theta          off-axis angle
     Output:
-        GGSO Patterns
+        GGSO gains :
+            Ggso     : in tangent plane 
+            GgsoP    : in perpendicular plane
     """
     Ggso = 0.0
-    GgsoT = 0.0
+    GgsoP = 0.0
 
-    if Theta > 48 and Theta <= 180:
+    Theta = abs(Theta)
+    if Theta > 48:
         Ggso = -10;
-        GgsoT = -10;
-        return Ggso, GgsoT
+        GgsoP = -10;
+        return Ggso, GgsoP
 
-    if Theta > 3 and Theta <= 48:
-        GgsoT = 32 - 25*math.log10(Theta)
-        if Theta <= 7:
-            Ggso = 29 - 25*math.log10(Theta)
-        if Theta > 7 and Theta <= 9.2:
-            Ggso = 8;
-        if Theta > 9.2:
-            Ggso = 32 - 25*math.log10(Theta)
-        return Ggso, GgsoT
-    
-    if Theta >= 1.5:
-        Ggso = 29 - 25*math.log10(Theta)
-    return Ggso, GgsoT
+    if Theta > 3:
+        GgsoP = 32 - 25*math.log10(Theta)
+
+    if Theta <= 1.5:
+        return Ggso, GgsoP
+    if Theta <= 7:
+        Ggso =  29 - 25*math.log10(Theta)
+    elif Theta <= 9.2:
+        Ggso = 8
+    else:
+        Ggso = 32 - 25*math.log10(Theta)
+ 
+    return Ggso, GgsoP
