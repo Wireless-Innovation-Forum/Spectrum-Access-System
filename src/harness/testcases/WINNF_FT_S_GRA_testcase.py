6160aaf02b99571b0b19b83f197ffb73c4946865
Harini Neelu Raghavan <harinisathya@gmail.com>
2017-10-17 12:53:24 -0700

    NEW Grant test case using new IDs: 2.
    grant_testcase.py → WINNF_FT_S_GRA_testcase.py
    Old GRA_7 → GRA_2

---
+++ src/harness/testcases/WINNF_FT_S_GRA_testcase.py
@@ -0,0 +1,127 @@
+#    Copyright 2016 SAS Project Authors. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Some parts of this software was developed by employees of
+# the National Institute of Standards and Technology (NIST),
+# an agency of the Federal Government.
+# Pursuant to title 17 United States Code Section 105, works of NIST employees
+# are not subject to copyright protection in the United States and are
+# considered to be in the public domain. Permission to freely use, copy,
+# modify, and distribute this software and its documentation without fee
+# is hereby granted, provided that this notice and disclaimer of warranty
+# appears in all copies.
+
+# THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER
+# EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY
+# THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM
+# INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE
+# SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE. IN NO EVENT
+# SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT,
+# INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM,
+# OR IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON
+# WARRANTY, CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED
+# BY PERSONS OR PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED
+# FROM, OR AROSE OUT OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES
+# PROVIDED HEREUNDER.
+
+# Distributions of NIST software should also include copyright and licensing
+# statements of any third-party software that are legally bundled with the
+# code in compliance with the conditions of those licenses.
+
+import json
+import os
+import sas
+import sas_testcase
+from util import winnforum_testcase, getRandomLatLongInPolygon, \
+  makePpaAndPalRecordsConsistent
+
+
+class GrantTestcase(sas_testcase.SasTestCase):
+
+  def setUp(self):
+    self._sas, self._sas_admin = sas.GetTestingSas()
+    self._sas_admin.Reset()
+
+  def tearDown(self):
+    pass
+
+  @winnforum_testcase
+  def test_WINNF_FT_S_GRA_2(self):
+    """Grant request array with various required parameters missing.
+
+    1. Missing cbsdId
+    2. Missing operationParam object
+    3. Missing maxEirp
+    4. Missing highFrequency
+    5. Missing lowFrequency
+
+    Returns 102 (MISSING_PARAM) for all requests.
+    """
+    # Register the devices
+    registration_request = []
+    for device_filename in ('device_a.json', 'device_c.json', 'device_e.json',
+                            'device_f.json', 'device_g.json'):
+      device = json.load(
+          open(os.path.join('testcases', 'testdata', device_filename)))
+      self._sas_admin.InjectFccId({'fccId': device['fccId']})
+      self._sas_admin.InjectUserId({'userId': device['userId']})
+      registration_request.append(device)
+    request = {'registrationRequest': registration_request}
+    response = self._sas.Registration(request)['registrationResponse']
+    # Check registration response
+    cbsd_ids = []
+    for resp in response:
+      self.assertEqual(resp['response']['responseCode'], 0)
+      cbsd_ids.append(resp['cbsdId'])
+    del request, response
+
+    # Prepare grant requests.
+    # 1. Missing cbsdId.
+    grant_0 = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    # 2. Missing operationParam object.
+    grant_1 = {'cbsdId': cbsd_ids[1]}
+    # 3. Missing maxEirp.
+    grant_2 = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_2['cbsdId'] = cbsd_ids[2]
+    del grant_2['operationParam']['maxEirp']
+    # 4. Missing highFrequency.
+    grant_3 = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_3['cbsdId'] = cbsd_ids[3]
+    del grant_3['operationParam']['operationFrequencyRange']['highFrequency']
+    # 5. Missing lowFrequency.
+    grant_4 = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_4['cbsdId'] = cbsd_ids[4]
+    del grant_4['operationParam']['operationFrequencyRange']['lowFrequency']
+
+    request = {'grantRequest': [grant_0, grant_1, grant_2, grant_3, grant_4]}
+    # Send grant request and get response
+    response = self._sas.Grant(request)['grantResponse']
+
+    self.assertEqual(len(response), 5)
+    # Check grant response # 1
+    self.assertFalse('cbsdId' in response[0])
+    self.assertFalse('grantId' in response[0])
+    self.assertEqual(response[0]['response']['responseCode'], 102)
+    # Check grant response # 2, 3, 4, 5
+    for response_num in (1, 2, 3, 4):
+      self.assertEqual(response[response_num]['cbsdId'], cbsd_ids[response_num])
+      self.assertFalse('grantId' in response[response_num])
+      self.assertEqual(response[response_num]['response']['responseCode'], 102)
+
+
