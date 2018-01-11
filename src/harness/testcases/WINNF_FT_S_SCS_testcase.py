6901f76a7dc8f8c3f404db749a1671791aa6ba39
jni2000 <james.ni@federatedwireless.com>
2018-01-11 14:17:16 -0500

    Federated Wireless: respond to review comments

--- src/harness/testcases/WINNF_FT_S_SCS_testcase.py
+++ src/harness/testcases/WINNF_FT_S_SCS_testcase.py
@@ -13,8 +13,10 @@
 #    limitations under the License.
 
 import security_testcase
-from util import winnforum_testcase
-import os
+from util import winnforum_testcase,countdown
+import os,time,json,sys,logging
+from OpenSSL import SSL,crypto
+
 
 class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   # Tests changing the SAS UUT state must explicitly call the SasReset().
@@ -32,7 +34,7 @@ class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   def test_WINNF_FT_S_SCS_2(self):
     """New registration with TLS_RSA_WITH_AES_256_GCM_SHA384 cipher.
 
-    Checks that SAS UUT response satisfy cipher security conditions.
+    Checks that SAS UUT response satisfy specific security conditions.
     Checks that a CBSD registration with this configuration succeed.
     """
     self.doTestCipher('AES256-GCM-SHA384')
@@ -41,7 +43,7 @@ class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   def test_WINNF_FT_S_SCS_3(self):
     """New registration with TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 cipher.
 
-    Checks that SAS UUT response satisfy cipher security conditions.
+    Checks that SAS UUT response satisfy specific security conditions.
     Checks that a CBSD registration with this configuration succeed.
     Note that the test require a SAS UUT
     """
@@ -51,7 +53,7 @@ class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   def test_WINNF_FT_S_SCS_4(self):
     """New registration with TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 cipher.
 
-    Checks that SAS UUT response satisfy cipher security conditions.
+    Checks that SAS UUT response satisfy specific security conditions.
     Checks that a CBSD registration with this configuration succeed.
     """
     self.doTestCipher('ECDHE-ECDSA-AES256-GCM-SHA384')
@@ -60,7 +62,7 @@ class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   def test_WINNF_FT_S_SCS_5(self):
     """New registration with TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 cipher.
 
-    Checks that SAS UUT response satisfy cipher security conditions.
+    Checks that SAS UUT response satisfy specific security conditions.
     Checks that a CBSD registration with this configuration succeed.
     """
     self.doTestCipher('ECDHE-RSA-AES128-GCM-SHA256')
@@ -68,53 +70,45 @@ class SasCbsdSecurityTestcase(security_testcase.SecurityTestCase):
   @winnforum_testcase
   def test_WINNF_FT_S_SCS_6(self):
     """Unrecognized root of trust certificate presented during registration.
-    Checks that SAS UUT responds with fatal alert with unknown_ca.
+    Checks that SAS UUT response with fatal alert with unknown_ca.
     """
-    device_cert = os.path.join('certs', 'unrecognized_device.cert')
-    device_key = os.path.join('certs', 'unrecognized_device.key')
+    device_cert = self.getCertFilename('unrecognized_device.cert')
+    device_key = self.getCertFilename('unrecognized_device.key')
     self.assertTlsHandshakeFailure(device_cert, device_key)
 
   @winnforum_testcase
   def test_WINNF_FT_S_SCS_7(self):
     """Corrupted certificate presented during registration.
-    Checks that SAS UUT responds with fatal alert message.
+    Checks that SAS UUT response with fatal alert message.
     """
-    with open(os.path.join('certs', 'client.cert')) as f:
-      data = "".join(f.read().split('-----END CERTIFICATE-----'))
-    def corrupt_signature(cert, offset):
-      temp = list(cert)
-      temp[len(cert) - offset] = chr(ord(temp[len(cert) - offset ]) + 1)
-      return ''.join(temp).strip() + '\n-----END CERTIFICATE-----\n'
-    with open(os.path.join('certs', 'corrupted_client.cert'),mode='w') as f:
-      f.write(corrupt_signature(data, 10))
-
-    device_cert = os.path.join('certs', 'corrupted_client.cert')
-    device_key = os.path.join('certs', 'client.key')
+    device_cert = self.getCertFilename('corrupted_client.cert')
+    device_key = self.getCertFilename('corrupted_client.key')
     self.assertTlsHandshakeFailure(device_cert, device_key)
 
   @winnforum_testcase
   def test_WINNF_FT_S_SCS_8(self):
     """Self-signed certificate presented during registration.
-    Checks that SAS UUT responds with fatal alert message.
+    Checks that SAS UUT response with fatal alert message.
     """
-    device_cert = os.path.join('certs', 'self_signed_client.cert')
-    device_key = os.path.join('certs', 'client.key')
+    device_cert = self.getCertFilename('self_signed_client.cert')
+    device_key = self.getCertFilename('client.key')
     self.assertTlsHandshakeFailure(device_cert, device_key)
 
   @winnforum_testcase
   def test_WINNF_FT_S_SCS_9(self):
     """Non-CBRS trust root signed certificate presented during registration.
-    Checks that SAS UUT responds with fatal alert message.
+    Checks that SAS UUT response with fatal alert message.
     """
-    device_cert = os.path.join('certs', 'non_cbrs_signed_device.cert')
-    device_key = os.path.join('certs', 'non_cbrs_signed_device.key')
+    device_cert = self.getCertFilename('non_cbrs_signed_device.cert')
+    device_key = self.getCertFilename('non_cbrs_signed_device.key')
     self.assertTlsHandshakeFailure(device_cert, device_key)
 
   @winnforum_testcase
   def test_WINNF_FT_S_SCS_10(self):
     """Certificate of wrong type presented during registration.
-    Checks that SAS UUT responds with fatal alert message.
+    Checks that SAS UUT response with fatal alert message.
     """
-    device_cert = os.path.join('certs', 'sas_ca_signed_client.cert')
-    device_key = os.path.join('certs', 'client.key')
+    device_cert = self.getCertFilename('sas_ca_signed_client.cert')
+    device_key = self.getCertFilename('client.key')
     self.assertTlsHandshakeFailure(device_cert, device_key)
+
