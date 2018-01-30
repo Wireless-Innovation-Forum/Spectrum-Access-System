4237530e7f2a01084fb7e398f7269994e17c7c42
Harini Neelu Raghavan <harinisathya@gmail.com>
2018-01-30 13:05:16 -0800

    New DRG testcase using new ID: 6
    Also includes the default.config
    
    Note:
    1. Need clarification on Step 5: CHECK: "If the corresponding request
    contained a valid cbsdId the response shall contain the same cbsdId".
    (I'll make the change in Line 410, once there is clarity. Right now the
    test fails at that point, expecting a 103 and the actual being 102).
    
    2. util.py is the same as PR #407

--- src/harness/testcases/WINNF_FT_S_DRG_testcase.py
+++ src/harness/testcases/WINNF_FT_S_DRG_testcase.py
@@ -13,11 +13,14 @@
 #    limitations under the License.
 from datetime import datetime
 import json
+import logging
 import os
 
 import sas
 import sas_testcase
-from util import winnforum_testcase
+from util import winnforum_testcase, configurable_testcase, writeConfig, \
+  loadConfig, addCbsdIdsToRequests, addGrantIdsToRequests
+
 
 class DeregistrationTestcase(sas_testcase.SasTestCase):
 
@@ -66,7 +69,7 @@ class DeregistrationTestcase(sas_testcase.SasTestCase):
       self.assertEqual(response[x]['response']['responseCode'], 0)
     del request, response
 
-     # Prepare grant requests for two cbsdIds
+    # Prepare grant requests for two cbsdIds
     grant_1 = json.load(open(os.path.join('testcases', 'testdata', 'grant_0.json')))
     grant_1['cbsdId'] = cbsd_ids[0]
     grant_2 = json.load(open(os.path.join('testcases', 'testdata', 'grant_0.json')))
@@ -123,7 +126,7 @@ class DeregistrationTestcase(sas_testcase.SasTestCase):
     self.assertEqual(response[0]['response']['responseCode'], 0)
     self.assertFalse('cbsdId' in response[1])
     self.assertEqual(response[1]['response']['responseCode'], 102)
-    
+
   def test_WINNF_FT_S_DRG_3(self):
     """CBSD ID initially exists, CBSD deregisters first by sending
     Deregistration request. Then sends another Deregistration request
@@ -233,7 +236,7 @@ class DeregistrationTestcase(sas_testcase.SasTestCase):
             'operationState': 'GRANTED'
         }]
     }
-   
+
     response = self._sas.Heartbeat(request)['heartbeatResponse'][0]
     # Check the heartbeat response
     self.assertEqual(response['cbsdId'], cbsd_id)
@@ -269,8 +272,161 @@ class DeregistrationTestcase(sas_testcase.SasTestCase):
         }]
     }
     response = self._sas.Heartbeat(request)['heartbeatResponse'][0]
-    
+
     # Check the heartbeat response
     self.assertEqual(response['response']['responseCode'], 103)
     del request, response
 
+  def generate_DRG_6_default_config(self, filename):
+    """Generates the WinnForum configuration for DRG.6."""
+
+    # Load device info
+    device_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_a.json')))
+    device_c = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_c.json')))
+    device_b = json.load(
+        open(os.path.join('testcases', 'testdata', 'device_b.json')))
+
+    # Load grant.json
+    grant_a = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_c = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+    grant_b = json.load(
+        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
+
+    # Heartbeat requests (where testcode will insert cbsdId and grantId)
+    heartbeat_a = {'operationState': 'GRANTED'}
+    heartbeat_c = {'operationState': 'GRANTED'}
+    heartbeat_b = {'operationState': 'GRANTED'}
+
+    # Deregistration requests (where testcode will fill in cbsdId)
+    deregister_a = {}
+    # device_c's CBSD ID will be removed due to the 'REMOVE' keyword.
+    deregister_c = {'cbsdId': 'REMOVE'}
+    deregister_b = {}
+
+    # Device_a and device_c are Category A.
+    self.assertEqual(device_a['cbsdCategory'], 'A')
+    self.assertEqual(device_c['cbsdCategory'], 'A')
+
+    # Device_b is Category B with conditionals pre-loaded.
+    self.assertEqual(device_b['cbsdCategory'], 'B')
+    conditionals_b = {
+        'cbsdCategory': device_b['cbsdCategory'],
+        'fccId': device_b['fccId'],
+        'cbsdSerialNumber': device_b['cbsdSerialNumber'],
+        'airInterface': device_b['airInterface'],
+        'installationParam': device_b['installationParam']
+    }
+    conditionals = {'registrationData': [conditionals_b]}
+    del device_b['installationParam']
+
+    # Create the actual config.
+    devices = [device_a, device_c, device_b]
+    grant_requests = [grant_a, grant_c, grant_b]
+    heartbeat_requests = [heartbeat_a, heartbeat_c, heartbeat_b]
+    deregistration_requests = [deregister_a, deregister_c, deregister_b]
+    config = {
+        'registrationRequest': devices,
+        'conditionalRegistrationData': conditionals,
+        'grantRequest': grant_requests,
+        'heartbeatRequest': heartbeat_requests,
+        'deregistrationRequest': deregistration_requests,
+        # First request, all valid params => SUCCESS
+        # Second request is missing CBSD ID => MISSING_PARAM
+        # Third request, all valid params => SUCCESS
+        'expectedResponseCodes': [(0,), (102,), (0,)]
+    }
+    writeConfig(filename, config)
+
+  @configurable_testcase(generate_DRG_6_default_config)
+  def test_WINNF_FT_S_DRG_6(self, config_filename):
+    """[Configurable] Array Deregistration."""
+
+    config = loadConfig(config_filename)
+    # Very light checking of the config file.
+    self.assertTrue(
+        len(config['registrationRequest']) == len(config['grantRequest']) ==
+        len(config['heartbeatRequest']) == len(config['deregistrationRequest']))
+    self.assertEqual(
+        len(config['deregistrationRequest']),
+        len(config['expectedResponseCodes']))
+
+    # Whitelist FCC IDs.
+    for device in config['registrationRequest']:
+      self._sas_admin.InjectFccId({
+          'fccId': device['fccId'],
+          'fccMaxEirp': 47
+      })
+
+    # Whitelist user IDs.
+    for device in config['registrationRequest']:
+      self._sas_admin.InjectUserId({'userId': device['userId']})
+
+    # Step 2 & 3: Register devices and get grants
+    cbsd_ids, grant_ids = self.assertRegisteredAndGranted(
+        config['registrationRequest'], config['grantRequest'],
+        config['conditionalRegistrationData'])
+
+    # Step 4: First Heartbeat Request
+    heartbeat_request = config['heartbeatRequest']
+    addCbsdIdsToRequests(cbsd_ids, heartbeat_request)
+    addGrantIdsToRequests(grant_ids, heartbeat_request)
+    request = {'heartbeatRequest': heartbeat_request}
+    responses = self._sas.Heartbeat(request)['heartbeatResponse']
+    self.assertEqual(len(responses), len(heartbeat_request))
+    # Check heartbeat response
+    for i, response in enumerate(responses):
+      self.assertEqual(response['response']['responseCode'], 0)
+    del request, responses
+
+    # Step 5: First deregistration request
+    degregister_request = config['deregistrationRequest']
+    addCbsdIdsToRequests(cbsd_ids, degregister_request)
+    request = {'deregistrationRequest': degregister_request}
+    responses = self._sas.Deregistration(request)['deregistrationResponse']
+    # Check the deregistration response
+    self.assertEqual(len(responses), len(config['expectedResponseCodes']))
+    for i, response in enumerate(responses):
+      expected_response_codes = config['expectedResponseCodes'][i]
+      logging.debug('Looking at response number %d', i)
+      logging.debug('Expecting to see response code in set %s in response: %s',
+                    expected_response_codes, response)
+      self.assertIn(response['response']['responseCode'],
+                    expected_response_codes)
+      if response['response']['responseCode'] == 0:
+        self.assertTrue('cbsdId' in response)
+        self.assertEqual(response['cbsdId'],
+                         degregister_request[i]['cbsdId'])
+      else:
+        self.assertFalse('cbsdId' in response)
+
+    # Step 6: Send the Deregistration request from Step 5 again
+    request = {'deregistrationRequest': degregister_request}
+    responses = self._sas.Deregistration(request)['deregistrationResponse']
+    # Check the deregistration response
+    self.assertEqual(len(responses), len(degregister_request))
+    for i, response in enumerate(responses):
+      # Expecting responseCode 103 and INVALID_VALUE for CBSD ID.
+      logging.debug('Looking at response number %d, response: %s', i, response)
+      self.assertEqual(response['response']['responseCode'], 103)
+      self.assertFalse('cbsdId' in response)
+    del request, responses
+
+    # Step 7: Send registration request from Step 2
+    cbsd_ids = self.assertRegistered(config['registrationRequest'],
+                                     config['conditionalRegistrationData'])
+
+    # Step 8: Heartbeat Request
+    heartbeat_requests = config['heartbeatRequest']
+    addCbsdIdsToRequests(cbsd_ids, heartbeat_requests)
+    request = {'heartbeatRequest': heartbeat_requests}
+    responses = self._sas.Heartbeat(request)['heartbeatResponse']
+    self.assertEqual(len(responses), len(heartbeat_request))
+    # Check heartbeat response
+    for i, response in enumerate(responses):
+      logging.debug('Looking at response number %d', i)
+      logging.debug('Actual response: %s', response)
+      self.assertTrue(response['response']['responseCode'] in [103, 500])
