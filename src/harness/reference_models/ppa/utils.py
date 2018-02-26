fcb3f7a7f2ccd3d5e892c9648b864105c61890ad
arhannan <ariful.hannan@commscope.com>
2018-02-26 12:22:35 -0500

    simplifying the ComputePPAProtectionPoints
    Thanks sbdt-google for the suggestions

--- src/harness/reference_models/ppa/utils.py
+++ src/harness/reference_models/ppa/utils.py
@@ -1,35 +1,26 @@
 import numpy as np
 from shapely import geometry
-
-def ComputePPAProtectionPoints(boundarysPoints, ARCSEC):   
+ 
+def ComputePPAProtectionPoints(boundary_points, resolution_arcsec):   
     """Gets all the protected points in a PPA.
      Inputs:
         boundary_points: a list of (lon,lat) tuple defining the boundary of the points.
-        res_arcsec: the gridding resolution (in arcsec).
-    
+        resolution_arcsec: the gridding resolution (in arcsec).
+     
      Returns:
-       all the protection points
-    """    
-    poly = geometry.Polygon([[lon,lat] for lon,lat in boundarysPoints])
-    arcsec = ARCSEC/3600.0
-    minlon, minlat, maxlon, maxlat = poly.bounds
-    
-    iLat = np.floor(minlat)
-    iLon = np.floor(minlon)
-    nL = np.floor((minlat - iLat)/arcsec)
-    nH = np.ceil((maxlat - iLat)/arcsec)
-    mL = np.floor((minlon - iLon)/arcsec)
-    mH = np.ceil((maxlon - iLon)/arcsec)
-        
-    latgrid = np.arange(iLat + nL*arcsec, iLat + nH*arcsec, arcsec)
-    longrid = np.arange(iLon + mL*arcsec, iLon + mH*arcsec, arcsec)
-    
-    ppa_points = []
-    if len(latgrid) > 0 and len(latgrid)>0:
-        for lat in latgrid:
-            for lon in longrid:
-                ppa_check_point = geometry.Point(lon, lat)
-                if ppa_check_point.within(poly):
-                        ppa_points.append([lat, lon])
-
-    return ppa_points
+       all the protection points within the contour
+    """   
+    ppa_contour_poly = geometry.Polygon([[lon,lat] for lon,lat in boundary_points])
+    resolution = resolution_arcsec/3600.0
+    lng_min, lat_min, lng_max, lat_max = ppa_contour_poly.bounds
+    lat_min = np.floor(lat_min / resolution) * resolution
+    lng_min = np.floor(lng_min / resolution) * resolution
+    lat_max = np.ceil(lat_max / resolution + 1) * resolution
+    lng_max = np.ceil(lng_max / resolution + 1) * resolution    
+    mesh_x, mesh_y = np.mgrid[lng_min:lng_max:resolution, lat_min:lat_max:resolution]
+    points = np.vstack((mesh_x.ravel(), mesh_y.ravel())).T
+    ppa_points = ppa_contour_poly.intersection(geometry.asMultiPoint(points))
+    if ppa_points.geom_type == 'MultiPoint':
+        return [[pt.x, pt.y] for pt in ppa_points]
+    elif ppa_points.geom_type == 'Point':
+        return [[ppa_points.x, ppa_points.y]] 
