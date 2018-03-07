ec998bcbc68ccae53c661c390f8a36d9ebe727f9
James Ni <james.ni@federatedwireless.com>
2018-03-07 10:14:17 -0500

    take out not related changes for other test cases

--- src/harness/testcases/WINNF_FT_S_SSS_testcase.py
+++ src/harness/testcases/WINNF_FT_S_SSS_testcase.py
@@ -16,10 +16,9 @@ import logging
 import os
 from OpenSSL import SSL
 from OpenSSL.crypto import load_certificate, FILETYPE_PEM
-from fake_crl_server import FakeCrlServerTestHarness
 import security_testcase
 from util import winnforum_testcase, configurable_testcase, writeConfig, loadConfig,\
-countdown getCertificateFingerprint
+getCertificateFingerprint
 
 SAS_CERT = os.path.join('certs', 'sas.cert')
 SAS_KEY = os.path.join('certs', 'sas.key')
@@ -217,54 +216,6 @@ class SasToSasSecurityTestcase(security_testcase.SecurityTestCase):
          self.assertEqual(e.args[0], 403)
       self.assertFalse(trigger_succeed, "Full Activity Dump is expected to fail")
 
-  def generate_SSS_11_default_config(self, filename):
-    """Generates the WinnForum configuration for SSS_11. """
-    # Create the configuration for blacklisted sas cert/key,wait timer & fake CRL Server information
-
-    config = {
-      'sasCert': self.getCertFilename("blacklisted_sas.cert"),
-      'sasKey' : self.getCertFilename("blacklisted_sas.key"),
-      'crlServer' : {
-                    'hostName':'localhost',
-                    'port':'9006',
-                    },
-      'crlUrl' : "ca.crl",
-      'crlFile': self.getCertFilename("ca_crl.crl"),
-      'wait_timer':60
-      }
-    writeConfig(filename, config)
-
-  @configurable_testcase(generate_SSS_11_default_config)
-  def test_WINNF_FT_S_SSS_11(self,config_filename):
-    """Blacklisted certificate presented during registration..
-       Checks that SAS UUT response with fatal alert message.
-    """
-    # Read the configuration
-    config = loadConfig(config_filename)
-
-    # Create the Fake CRL Server
-    self.fake_crl_server = FakeCrlServerTestHarness(
-                           config['crlServer']['hostName'],
-                           config['crlServer']['port'],
-                           config['crlUrl'],
-                           config['crlFile'])
-
-    # Start the Fake CRL Server
-    self.fake_crl_server.start()
-
-    logging.info("Waiting for %s secs to allow the UUT to pull the revoked certificate "
-                 "list from the fake CRL server " % config['wait_timer'])
-
-    # Wait for the timer
-    countdown(config['wait_timer'])
-
-    # Tls handshake fails
-    self.assertTlsHandshakeFailure(config['sasCert'], config['sasKey'])
-    logging.info("TLS handshake failed as the sas certificate has blacklisted")
-
-    # Stop the Fake CRL Server
-    self.fake_crl_server.stopServer()
-
   def generate_SSS_12_default_config(self, filename):
     """Generates the WinnForum configuration for SSS.12"""
     # Create the actual config for SAS cert/key path
@@ -380,54 +331,6 @@ class SasToSasSecurityTestcase(security_testcase.SecurityTestCase):
        # Check if HTTP status is 403
        self.assertEqual(e.args[0], 403)
     self.assertFalse(trigger_succeed, "Full Activity Dump is expected to fail")
-
-  def generate_SSS_16_default_config(self, filename):
-    """Generates the WinnForum configuration for SSS_16. """
-    # Create the configuration for client cert/key,wait timer & fake CRL Server information
-
-    config = {
-      'sasCert': self.getCertFilename("sas.cert"),
-      'sasKey' : self.getCertFilename("sas.key"),
-      'crlServer' : {
-                    'hostName':'localhost',
-                    'port':'9006',
-                    },
-      'crlUrl' : "ca.crl",
-      'crlFile': self.getCertFilename("ca_crl.crl"),
-      'wait_timer':60
-      }
-    writeConfig(filename, config)
-
-  @configurable_testcase(generate_SSS_16_default_config)
-  def test_WINNF_FT_S_SSS_16(self,config_filename):
-    """Certificate signed by a revoked CA presented during registration.
-       Checks that SAS UUT response with fatal alert message.
-    """
-    # Read the configuration
-    config = loadConfig(config_filename)
-
-    # Create the Fake CRL Server
-    self.fake_crl_server = FakeCrlServerTestHarness(
-                           config['crlServer']['hostName'],
-                           config['crlServer']['port'],
-                           config['crlUrl'],
-                           config['crlFile'])
-
-    # Start the Fake CRL Server
-    self.fake_crl_server.start()
-
-    logging.info("Waiting for %s secs to allow the UUT to pull the revoked certificate "
-                 "list from the fake CRL server " % config['wait_timer'])
-
-    # Wait for the timer
-    countdown(config['wait_timer'])
-
-    # Tls handshake fails since CA is revoked
-    self.assertTlsHandshakeFailure(config['sasCert'], config['sasKey'])
-    logging.info("TLS handshake failed as the CA certificate has been revoked")
-
-    # Stop the Fake CRL Server
-    self.fake_crl_server.stopServer()
 
   def generate_SSS_17_default_config(self, filename):
     """Generates the WinnForum configuration for SSS_17"""
