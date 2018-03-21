5c1b35f13ee39b844ca2d627a1a151164aaa11cc
jni2000 <james.ni@federatedwireless.com>
2018-03-21 17:41:26 -0400

    revision in response to review commnets as of mar. 20

--- src/harness/reference_models/pre_iap_filtering/pre_iap_filtering.py
+++ src/harness/reference_models/pre_iap_filtering/pre_iap_filtering.py
@@ -14,16 +14,16 @@
 
 """
 ==================================================================================
-  This is the Pre-IAP reference model which implements calling of 4 sub reference
-  modules in to ensure the checks to be done before sending to the IAP calculation.
+  This is the main Pre-IAP reference model which invokes the sub pre-IAP reference
+  models to filter out grants and CBSDs before IAP model is invoked.
 ==================================================================================
 """
-
+import copy
 from reference_models.fss_purge import fss_purge
 from reference_models.inter_sas_duplicate_grant import inter_sas_duplicate_grant
 
 
-def performPreIapFiltering(protected_entities, uut_fad_object, test_harness_fad_object):
+def performPreIapFiltering(protected_entities, input_sas_uut_fad, input_sas_test_harness_fads):
   """ The main function that invokes all pre-IAP filtering models.
 
   The grants/CBSDs to be purged are removed from the input parameters.
@@ -32,20 +32,28 @@ def performPreIapFiltering(protected_entities, uut_fad_object, test_harness_fad_
     protected_entities: : A dictionary containing the list of protected entities. The key
       is a protected enity type and the value is a list of corresponding protected
       entity records. The format is {'entity_name':[record1, record2]}.
-    uut_fad_object: A FullActivityDump object containing the FAD records of SAS UUT.
-    test_harness_fad_objects: A list of FullActivityDump objects containing the FAD records
+    input_sas_uut_fad_object: A FullActivityDump object containing the FAD records of SAS UUT.
+    input_sas_test_harness_fads: A list of FullActivityDump objects containing the FAD records
       from SAS test harnesses.
+  Returns:
+    uut_fad_after_fss_purge: A FullActivityDump object containing the FAD record of SAS UUT 
+      after purged grants removed.
+    test_harness_fads_after_fss_purge: A list of FullActivityDump objects containing the FAD 
+      record of SAS test harness after purged grants removed.
   """    
 
-  # Invoke Inter SAS duplicate grant purge list reference model
-  inter_sas_duplicate_grant.interSasDuplicateGrantReferenceModel\
-                                (uut_fad_object, test_harness_fad_object)
+  sas_uut_fad_object = copy.deepcopy(input_sas_uut_fad)
+  test_harness_fad_objects = copy.deepcopy(input_sas_test_harness_fads)
 
+  # Invoke Inter SAS duplicate grant purge list reference model
+  uut_fad_after_duplicate_removal, test_harness_fads_after_duplicate_removal = inter_sas_duplicate_grant.interSasDuplicateGrantReferenceModel\
+                                (sas_uut_fad_object, test_harness_fad_objects)
+
+  # TODO
   # Invoke PPA, EXZ, GWPZ, and FSS+GWBL purge list reference models
 
   # Invoke FSS purge list reference model
-  for entity in protected_entities:
-    if 'fssRecords' in entity:
-      for fss_record in entity['fssRecords']:
-        fss_purge.fssPurgeModel(uut_fad_object, test_harness_fad_object, fss_record)
-
+  uut_fad_after_fss_purge, test_harness_fads_after_fss_purge = fss_purge.fssPurgeModel(
+                        uut_fad_after_duplicate_removal, test_harness_fads_after_duplicate_removal,
+                        protected_entities['fssRecords'])
+  return uut_fad_after_fss_purge, test_harness_fads_after_fss_purge
