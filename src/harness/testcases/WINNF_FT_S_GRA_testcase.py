9310b6eb76169e9536f537a7b55841b875b6fe9a
redtechnologies <rhossino@redtechnologies.fr>
2018-03-27 14:40:26 +0200

    # remove unapproved comment
    # update time value of wait for DPA
    # update step number

--- src/harness/testcases/WINNF_FT_S_GRA_testcase.py
+++ src/harness/testcases/WINNF_FT_S_GRA_testcase.py
@@ -103,7 +103,7 @@ class GrantTestcase(sas_testcase.SasTestCase):
     self._sas_admin.TriggerDpaActivation(\
         {'frequencyRange': {'lowFrequency': pal_low_frequency, \
                            'highFrequency': pal_high_frequency}, 'dpaId': 'east_dpa4'})
-    # UNAPPROVED Not in WINNF-TS-0016 Release 1 Spec, but necessary Step for DPA
+    # wait for DPA activation
     time.sleep(240)
     # Send grant request
     grant_0 = json.load(
