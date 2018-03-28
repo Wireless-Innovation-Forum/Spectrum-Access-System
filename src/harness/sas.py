401d734d4ea6c682f758caab0a636db36b3d4f3e
jni2000 <james.ni@federatedwireless.com>
2018-03-28 15:55:34 -0400

    reverted sas.py

--- src/harness/sas.py
+++ src/harness/sas.py
@@ -94,41 +94,6 @@ class SasImpl(sas_interface.SasInterface):
   def _GetDefaultSasSSLKeyPath(self):
     return os.path.join('certs', 'client.key')
 
-  def DownloadFile(self, url, ssl_cert=None, ssl_key=None):
-    return self._DownloadFile('%s' % url,
-                 ssl_cert if ssl_cert else self._GetDefaultSasSSLCertPath(),
-                 ssl_key if ssl_key else self._GetDefaultSasSSLKeyPath())
-
-  def _DownloadFile(self, url, ssl_cert, ssl_key):
-    CIPHERS = [
-          'AES128-GCM-SHA256',  # TLS_RSA_WITH_AES_128_GCM_SHA256
-          'AES256-GCM-SHA384',  # TLS_RSA_WITH_AES_256_GCM_SHA384
-          'ECDHE-RSA-AES128-GCM-SHA256',  # TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
-      ]
-    response = StringIO.StringIO()
-    conn = pycurl.Curl()
-    conn.setopt(conn.URL, url)
-    conn.setopt(conn.WRITEFUNCTION, response.write)
-    header = [
-          'Host: %s' % urlparse.urlparse(url).hostname,
-          'content-type: application/json'
-    ]
-    conn.setopt(conn.VERBOSE, 3)
-    conn.setopt(conn.SSLVERSION, conn.SSLVERSION_TLSv1_2)
-    conn.setopt(conn.SSLCERTTYPE, 'PEM')
-    conn.setopt(conn.SSLCERT, ssl_cert)
-    conn.setopt(conn.SSLKEY, ssl_key)
-    conn.setopt(conn.CAINFO, "/etc/ssl/certs/ca.cert")
-    conn.setopt(conn.HTTPHEADER, header)
-    conn.setopt(conn.SSL_CIPHER_LIST, ':'.join(CIPHERS))
-    logging.debug('Request to URL ' + url)
-    conn.setopt(conn.TIMEOUT, HTTP_TIMEOUT_SECS)
-    conn.perform()
-    assert conn.getinfo(pycurl.HTTP_CODE) == 200, conn.getinfo(pycurl.HTTP_CODE)
-    conn.close()
-    body = response.getvalue()
-    logging.debug('Response:\n' + body)
-    return json.loads(body)
 
 class SasAdminImpl(sas_interface.SasAdminInterface):
   """Implementation of SasAdminInterface for SAS certification testing."""
@@ -226,6 +191,10 @@ class SasAdminImpl(sas_interface.SasAdminInterface):
   def TriggerDailyActivitiesImmediately(self):
     RequestPost('https://%s/admin/trigger/daily_activities_immediately' %
                 self._base_url, None, self._tls_config)
+
+  def QueryPropagationAndAntennaModel(self, request):
+    return RequestPost('https://%s/admin/query/propagation_and_antenna_model' %
+                 self._base_url, request, self._tls_config)
 
   def GetDailyActivitiesStatus(self):
     return RequestPost(
