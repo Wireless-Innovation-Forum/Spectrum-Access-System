Fix interference calculation.
Also add the cache for more robust checks.
Remove the changes to interference.py which will be done on another
cleaning PR. Update the examples.
Put back the old legacu findMoveList routine.

src/harness/reference_models/move_list_calc/dpa_mgr.py
@@ -18,10 +18,7 @@ This DPA manager manages the move list and interference calculation for a DPA.
 DPA differs from other protection entities as the aggregated interference is
 computed at specific percentile of the combined interference random variable.
 """
-
-from collections import namedtuple
-from functools import partial
-import multiprocessing
+import functools
 
 import numpy as np
 
@@ -37,22 +34,16 @@ DPA_DEFAULT_THRESHOLD_PER_10MHZ = -144
 DPA_CHANNEL_BANDWIDTH = 10
 
 
-# The protection specs, as required by move_list calc routine.
-ProtectionSpecs = namedtuple('ProtectionSpecs',
-                             ['lowFreq', 'highFreq',
-                              'antHeight', 'beamwidth', 'threshold'])
-
-
 class Dpa(object):
   """Dynamic Protection Area.
 
-  A dynamic protection area is an ESC/Portal triggered protection area.
+  A dynamic protection area is an ESC/Portal triggered protection area, ie not always on.
   There are 2 main types of DPA: Coastal DPAs and Inland DPAs.
   The difference between the two are mainly the set of parameters to use for
   its protection.
 
-  Most DPA are co-channel DPA, although a few are Out-Of-Band for which special
-  protection logic is run.
+  Most DPA are co-channel only, although a few have also Out-Of-Band protected channels
+  for which special protection logic is run.
 
   Attributes:
     channels: The list of (freq_min_mhz, freq_max_mhz) to protect for that DPA.
@@ -63,13 +54,23 @@ class Dpa(object):
     radar_height: The radar height (meters).
     beamwidth: The radar antenna beamwidth (degrees).
     azimuth_range: The radar azimuth range (degrees) as a tuple of
-      (min_azimuth, max_azimuth) relative to true north.
+      (min_azimuth, max_azimuth) relative to true north. UNUSED for now
 
     grants: The list of registered grants.
     move_lists: A list of move list (list of |CbsdGrantInfo|) per channel.
     keep_lists: A list of keep list (list of |CbsdGrantInfo|) per channel.
+
+  Usage:
+    # Setup the DPA
+    dpa = Dpa(protected_points)
+    dpa.SetGrantsFromFad(sas_uut_fad, sas_th_fads)
+    # Compute the move list
+    dpa.ComputeMoveLists()
+    # Calculate the keep list interference for a given channel
+    interf_per_point = dpa.CalcKeepListInterference(channel)
   """
-  # TODO(sbdt): review the holistic usage and definition across test harness
+  # TODO(sbdt): add internal support for azimuth range, since it is used by some
+  # inland DPAs.
   num_iteration = 2000
   protection_zone = zones.GetCoastalProtectionZone()
 
@@ -125,13 +126,18 @@ class Dpa(object):
     self.grants = grants
     self.ResetLists()
 
-  def CalcMoveLists(self):
-    """Calculate move/keep lists.
+  def ComputeMoveLists(self):
+    """Computes move/keep lists.
+
+    This routine updates the internal grants move list and neighbor list. One set of list
+    is maintained per protected channel.
+    To retrieve the list, see the routines GetMoveList(), GetNeighborList() and
+    GetKeepList().
     """
     pool = mpool.Pool()
     self.ResetLists()
     for low_freq, high_freq in self.channels:
-      moveListConstraint = partial(
+      moveListConstraint = functools.partial(
           ml.moveListConstraint,
           low_freq=low_freq * 1.e6,
           high_freq=high_freq * 1.e6,
@@ -158,19 +164,74 @@ class Dpa(object):
       raise ValueError('Channel {} not protected by this DPA'.format(channel))
     return chan_idx
 
+  def GetMoveList(self, channel):
+    """Returns the move list for a given channel, as a set of grants.
+
+    Args:
+      channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
+    """
+    return self.move_lists[self._GetChanIdx(channel)]
+
+  def GetNeighborList(self, channel):
+    """Returns the neighbor list for a given channel, as a set of grants.
+
+    Args:
+      channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
+    """
+    return self.nbor_lists[self._GetChanIdx(channel)]
+
+  def GetKeepList(self, channel):
+    """Returns the keep list for a given channel, as a set of grants.
+
+    Args:
+      channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
+    """
+    return self.GetNeighborList(channel).difference(self.GetMoveList(channel))
+
   def GetMoveListMask(self, channel):
     """Returns move list mask as a vector of bool.
 
     Args:
       channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
     """
-    # Legacy function for old example.
-    move_list = self.move_lists[self._GetChanIdx(channel)]
+    # Legacy function for getting a mask, as used in some example code.
+    move_list = self.GetMoveList(channel)
     mask = np.zeros(len(self.grants), np.bool)
     for k, grant in enumerate(self.grants):
       if grant in move_list:
         mask[k] = True
     return mask
+
+  def CalcKeepListInterference(self, channel, num_iter=None):
+    """Calculates max aggregate interference per protected point.
+
+    Args:
+      channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
+      num_iter: The number of Monte Carlo iteration when calculating the aggregate
+        interference.
+
+    Returns:
+      The 95% aggregate interference per protected point, as a list.
+      Each element is the maximum aggregate interference over all radar direction.
+    """
+    if num_iter is None:
+      num_iter = Dpa.num_iteration
+    keep_list = self.GetKeepList(channel)
+    interfCalculator = functools.partial(
+        ml.calcAggregatedInterference,
+        low_freq=channel[0] * 1e6,
+        high_freq=channel[1] * 1e6,
+        grants=keep_list,
+        exclusion_zone=Dpa.protection_zone,
+        inc_ant_height=self.radar_height,
+        num_iter=num_iter,
+        beamwidth=self.beamwidth,
+        do_max=True)
+
+    pool = mpool.Pool()
+    max_interf = list(pool.map(interfCalculator,
+                               self.protected_points))
+    return max_interf
 
   def CheckInterference(self, channel, sas_uut_active_grants, margin_db, num_iter=None):
     """Checks interference of keep list of SAS UUT vs test harness.
@@ -182,7 +243,7 @@ class Dpa(object):
 
     Args:
       channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
-      sas_uut_active_grants: A list of authorized |data.CbsdGrantInfo| grants
+      sas_uut_active_grants: An iterable of authorized |data.CbsdGrantInfo| grants.
         of the SAS UUT (ie not in the move list).
       margin_db: The margin (in dB) to use for the check.
       num_iter: The number of Monte Carlo iteration for calculating the aggregate
@@ -192,55 +253,41 @@ class Dpa(object):
       True if all SAS UUT aggregated interference are within margin, False otherwise
       (ie if at least one combined protection point / azimuth fails the test).
     """
-    chan_idx = self._GetChanIdx(channel)
-    move_list = self.move_lists[chan_idx]
-    nborlist = self.nbor_lists[chan_idx]
+    keep_list = self.GetKeepList(channel)
+    nbor_list = self.GetNeighborList(channel)
     if num_iter is None:
       num_iter = Dpa.num_iteration
-
     # Find the keep list of TH and SAS UUT
     keep_list_th_other_sas = [
         grant for grant in self.grants
         if (not grant.is_managed_grant and
-            grant in nbor_list and
-            grant not in move_list)]
+            grant in keep_list)]
     keep_list_th_managing_sas = [
         grant for grant in self.grants
         if (grant.is_managed_grant and
-            grant in nbor_lists and
-            grant not in move_list)]
+            grant in keep_list)]
     keep_list_uut_managing_sas = [
         grant for grant in sas_uut_active_grants
         if (grant in nbor_list and
             grant not in keep_list_th_other_sas)]
 
-    def _CheckPointInterf(point):
-      th_interferences = ml.CalcInterference(
-          point,
-          low_freq=channel[0] * 1e6,
-          high_freq=channel[1] * 1e6,
-          grants=keep_list_th_other_sas + keep_list_th_managing_sas,
-          exclusion_zone=Dpa.protection_zone,
-          inc_ant_height=self.radar_height,
-          num_iter=num_iter,
-          beamwidth=self.beamwidth)
-      uut_interferences = ml.CalcInterference(
-          point,
-          low_freq=channel[0] * 1e6,
-          high_freq=channel[1] * 1e6,
-          grants=keep_list_th_other_sas + keep_list_uut_managing_sas,
-          exclusion_zone=Dpa.protection_zone,
-          inc_ant_height=self.radar_height,
-          num_iter=num_iter,
-          beamwidth=self.beamwidth)
-      max_diff_interf = np.max(uut_interferences - th_interferences)
-      if max_diff_interf > margin:
-        return False
-      return True
+    checkPointInterf = functools.partial(
+        _CalcTestPointInterfDiff,
+        channel=channel,
+        keep_list_th_other_sas=keep_list_th_other_sas,
+        keep_list_th_managing_sas=keep_list_th_managing_sas,
+        keep_list_uut_managing_sas=keep_list_uut_managing_sas,
+        radar_height=self.radar_height,
+        beamwidth=self.beamwidth,
+        protection_zone=Dpa.protection_zone,
+        num_iter=num_iter)
 
+    # TODO(sbdt): could do early stop as soon as one fails, although I expect
+    # the criteria to be changed into checking 99.x% of success instead of 100%.
     pool = mpool.Pool()
-    result = pool.map(_CheckPointInterf, self.protected_points)
-    return np.all(result)
+    result = list(pool.map(checkPointInterf, self.protected_points))
+    max_diff_interf = max(result)
+    return max_diff_interf <= margin_db
 
 
 def GetDpaProtectedChannels(freq_ranges_mhz):
@@ -255,9 +302,71 @@ def GetDpaProtectedChannels(freq_ranges_mhz):
   """
   channels = set()
   for freq_range in freq_ranges_mhz:
-    min_freq = int(freq_range[0] / 10.) * 10
-    max_freq = int(freq_range[1] / 10.) * 10
-    freqs = np.arange(min_freq, max_freq, 10)
+    min_freq = int(freq_range[0] / DPA_CHANNEL_BANDWIDTH) * DPA_CHANNEL_BANDWIDTH
+    max_freq = int(freq_range[1] / DPA_CHANNEL_BANDWIDTH) * DPA_CHANNEL_BANDWIDTH
+    freqs = np.arange(min_freq, max_freq, DPA_CHANNEL_BANDWIDTH)
     for freq in freqs:
-      channels.add((freq, freq+10))
+      channels.add((freq, freq + DPA_CHANNEL_BANDWIDTH))
   return sorted(list(channels))
+
+def _CalcTestPointInterfDiff(point,
+                             channel,
+                             keep_list_th_other_sas,
+                             keep_list_th_managing_sas,
+                             keep_list_uut_managing_sas,
+                             radar_height,
+                             beamwidth,
+                             protection_zone,
+                             num_iter):
+  """Calculate difference of aggregate interference between reference and SAS UUT.
+
+  This implements the check required by the IPR certification tests, comparing the
+  reference model keep list aggregated interference versus the blended one.
+  The blended one uses the SAS UUT authorized grants that it manages, plus the CBSD
+  of other SAS in the keep list.
+
+  Note that this routine reduce the amount of random variation by reusing the same
+  random draw for the CBSD that are shared between the two keep lists. This is done
+  by using the caching engine provided by |reference_models.common.cache|.
+
+  Args:
+    channel: A channel as tuple (low_freq_mhz, high_freq_mhz).
+    keep_list_th_other_sas: A list of |data.CbsdGrantInfo| for non managing SAS, as
+      computed by the reference model.
+    keep_list_th_managing_sas: A list of |data.CbsdGrantInfo| for managing SAS, as
+      computed by the reference model.
+    keep_list_uut_managing_sas: A list of |data.CbsdGrantInfo| for managing SAS as
+      computed by the SAS UUT.
+    radar_height: The radar height (meters).
+    beamwidth: The radar antenna beamwidth (degrees).
+    protection_zone: A |shapely.Polygon/MultiPolygon| defining the CatA protection zone.
+    num_iteration: The number of iteration to use in the Monte Carlo simulation.
+
+  Returns:
+    The maximum aggregated difference across all the radar pointing direction between
+    the blended and the reference models.
+  """
+  # Perform caching of the per device interference, as to reduce the Monte-Carlo
+  # variability on similar CBSD in keep list.
+  # TODO(sbdt): check if better to context manage once per process, with clearing
+  # in between.
+  with ml.InterferenceCacheManager() as cm:
+    th_interferences = ml.calcAggregatedInterference(
+        point,
+        low_freq=channel[0] * 1e6,
+        high_freq=channel[1] * 1e6,
+        grants=keep_list_th_other_sas + keep_list_th_managing_sas,
+        exclusion_zone=protection_zone,
+        inc_ant_height=radar_height,
+        num_iter=num_iter,
+        beamwidth=beamwidth)
+    uut_interferences = ml.calcAggregatedInterference(
+        point,
+        low_freq=channel[0] * 1e6,
+        high_freq=channel[1] * 1e6,
+        grants=keep_list_th_other_sas + keep_list_uut_managing_sas,
+        exclusion_zone=protection_zone,
+        inc_ant_height=radar_height,
+        num_iter=num_iter,
+        beamwidth=beamwidth)
+    return np.max(uut_interferences - th_interferences)

src/harness/reference_models/move_list_calc/move_list.py
@@ -34,18 +34,20 @@
 #   - 'calcInterference()': calculates the 95% quantile interference for one point
 #==================================================================================
 
+import functools
+from collections import namedtuple
+from enum import Enum
+
 import numpy as np
-from operator import attrgetter, iadd
 from shapely.geometry import Point as SPoint
 from shapely.geometry import Polygon as SPolygon
 from shapely.geometry import MultiPolygon as MPolygon
-from collections import namedtuple
-from enum import Enum
 
 # Import WINNF reference models including propagation, geo, and CBSD antenna gain models
 from reference_models.propagation import wf_itm
 from reference_models.geo import drive
 from reference_models.geo import vincenty
+from reference_models.common import cache
 from reference_models.common import data
 from reference_models.antenna import antenna
 
@@ -75,7 +77,7 @@ OOB_POWER_BELOW_3530MHZ = -40
 
 
 # Define interference contribution, i.e., a tuple with named fields of
-# 'grantIndex', 'medianInterference', 'randomInterference', 'bearing_c_cbsd'
+# 'randomInterference', 'bearing_c_cbsd'
 InterferenceContribution = namedtuple('InterferenceContribution',
                                       ['randomInterference', 'bearing_c_cbsd'])
 
@@ -251,7 +253,7 @@ def computeInterference(grant, constraint, inc_ant_height, num_iteration, dpa_ty
     the protection constraint c.
 
     Inputs:
-        cbsd_grant:     a |CbsdGrantInfo| grant
+        cbsd_grant:     a |data.CbsdGrantInfo| grant
         constraint: 	protection constraint of type |data.ProtectionConstraint|
         inc_ant_height: reference incumbent antenna height (in meters)
         num_iteration:  a number of Monte Carlo iterations
@@ -365,11 +367,6 @@ def formInterferenceMatrix(grants, grants_ids, constraint,
     I = np.array([interf_list[k].randomInterference for k in sorted_idxs]).transpose()
     sorted_bearings = np.array([interf_list[k].bearing_c_cbsd for k in sorted_idxs])
     sorted_grant_ids = [grants_ids[k] for k in sorted_idxs]
-    #interf_list.sort(key=attrgetter('medianInterference'))
-    # Get indices, bearings, interference contributions of the sorted grants
-    #sorted_grant_indices = [interf.grantIndex for interf in interf_list]
-    #sorted_bearings = [interf.bearing_c_cbsd for interf in interf_list]
-    #I = np.array([interf.randomInterference for interf in interf_list]).transpose()
     return I, sorted_grant_ids, sorted_bearings
 
 
@@ -438,7 +435,8 @@ def find_nc(I, bearings, t, beamwidth):
 
     return nc
 
-
+#------------------------------------------
+# Public interface below
 def moveListConstraint(protection_point, low_freq, high_freq,
                        grants,
                        exclusion_zone, inc_ant_height,
@@ -478,8 +476,6 @@ def moveListConstraint(protection_point, low_freq, high_freq,
     neighbor_grants, neighbor_idxs = findGrantsInsideNeighborhood(grants,
                                                                   constraint,
                                                                   exclusion_zone, dpa_type)
-    #import ipdb; ipdb.set_trace()
-
     if len(neighbor_grants):  # Found CBSDs in the neighborhood
         # Form the matrix of interference contributions
         I, sorted_neighbor_idxs, bearings = formInterferenceMatrix(
@@ -497,11 +493,11 @@ def moveListConstraint(protection_point, low_freq, high_freq,
     return ([], neighbor_idxs)
 
 
-def calcInterference(protection_point, low_freq, high_freq,
-                     grants,
-                     exclusion_zone, inc_ant_height,
-                     num_iter, beamwidth):
-  """Computes the 95% interference quantile on a protected point.
+def calcAggregatedInterference(protection_point, low_freq, high_freq,
+                               grants,
+                               exclusion_zone, inc_ant_height,
+                               num_iter, beamwidth, do_max=False):
+  """Computes the 95% aggregated interference quantile on a protected point.
 
     Inputs:
         protection_point:  A protection point location, having attributes 'latitude' and
@@ -514,13 +510,15 @@ def calcInterference(protection_point, low_freq, high_freq,
         inc_ant_height:    The reference incumbent antenna height (meters).
         num_iter:          The number of Monte Carlo iterations.
         beamwidth:         The protection antenna beamwidth (degree).
-
+        do_max:            If True, returns the maximum interference over all radar azimuth,
     Returns:
-        A vector of interference values (ndarray) of length N, where the element k
-        corresponds to azimuth k * (beamwith/2).
-        The length N is thus equal to 2 * 360 / beamwith.
+        The 95% aggregated interference (dB) either:
+          - as a vector (ndarray) of length N, where the element k corresponds to
+              azimuth k * (beamwith/2). The length N is thus equal to 2 * 360 / beamwith.
+          - the maximum over all radar azimuth, if `do_max` is set to True.
   """
   dpa_type = findDpaType(low_freq, high_freq)
+  if not beamwidth: beamwidth = 360
 
   # Assign values to the protection constraint
   constraint = data.ProtectionConstraint(latitude=protection_point.latitude,
@@ -531,21 +529,19 @@ def calcInterference(protection_point, low_freq, high_freq,
 
   # Identify CBSD grants in the neighborhood of the protection constraint
   neighbor_grants, _ = findGrantsInsideNeighborhood(grants, constraint,
-                                                 exclusion_zone, dpa_type)
+                                                    exclusion_zone, dpa_type)
+  if not neighbor_grants:
+    return np.asarray(-1000)
   interf_matrix = np.zeros((num_iter, len(neighbor_grants)))
   bearings = np.zeros(len(neighbor_grants))
-  for grant in neighbor_grants:
-    interf = computeInterference(grant, constraint, inc_ant_height,
-                                 num_iter, dpa_type)
-    interf_matrix[:,k] = inter.randomInterference
+  for k, grant in enumerate(neighbor_grants):
+    interf, _ = computeInterference(grant, constraint, inc_ant_height,
+                                    num_iter, dpa_type)
+    interf_matrix[:,k] = interf.randomInterference
     bearings[k] = interf.bearing_c_cbsd
 
   interf_matrix = 10**(interf_matrix / 10.)
-  if beamwidth == 360.0:
-    azimuths = [0]
-  else:
-    azimuths = np.arange(0.0, 360.0, beamwidth/2.0)
-
+  azimuths = [0] if beamwidth == 360 else np.arange(0.0, 360.0, beamwidth/2.0)
   agg_interf = np.zeros(len(azimuths))
   for k, azi in enumerate(azimuths):
     if beamwidth == 360.0:
@@ -555,9 +551,14 @@ def calcInterference(protection_point, low_freq, high_freq,
     dpa_interf = interf_matrix * 10**(dpa_gains / 10.0)
     agg_interf[k] = np.percentile(np.sum(dpa_interf, axis=1),
                                   PROTECTION_PERCENTILE, interpolation='lower')
+  agg_interf = 10 * np.log10(agg_interf)
+  return np.max(agg_interf) if do_max else agg_interf
 
-  return 10 * np.log10(agg_interf)
-
+# Cache management
+class InterferenceCacheManager(cache.CacheManager):
+  """Interference cache context manager."""
+  def __init__(self, maxsize=None):
+    super(InterferenceCacheManager, self).__init__(computeInterference, maxsize)
 
 #----------------------------------
 # Legacy routine, just to support existing client code using old interface.