b85e112c74a91c43c3e7e98baa136f89002d107c
jni2000 <james.ni@federatedwireless.com>
2018-04-25 08:38:54 -0400

    revision in response to review comments as of April 20

--- src/harness/testcases/WINNF_FT_S_WDB_testcase.py
+++ src/harness/testcases/WINNF_FT_S_WDB_testcase.py
@@ -17,9 +17,6 @@ import logging
 import os
 import sas
 import sas_testcase
-import signal
-import time
-from datetime import datetime
 from util import configurable_testcase, writeConfig, loadConfig,\
     convertRequestToRequestWithCpiSignature, makePalRecordsConsistent
 
@@ -32,98 +29,6 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
 
   def tearDown(self):
     pass
-
-  def triggerPpaCreationAndWaitUntilComplete(self, ppa_creation_request):
-    """Triggers PPA Creation Admin API and returns PPA ID if the creation status is completed.
-
-    Triggers PPA creation to the SAS UUT. Checks the status of the PPA creation
-    by invoking the PPA creation status API. If the status is complete then the
-    PPA ID is returned. The status is checked every 10 secs for upto 2 hours.
-    Exception is raised if the PPA creation returns error or times out.
-
-    Args:
-      ppa_creation_request: A dictionary with a multiple key-value pair containing the
-        "cbsdIds", "palIds" and optional "providedContour"(a GeoJSON object).
-
-    Returns:
-      A Return value is string format of the PPA ID.
-
-    """
-    ppa_id = self._sas_admin.TriggerPpaCreation(ppa_creation_request)
-
-    # Verify ppa_id should not be None.
-    self.assertIsNotNone(ppa_id, msg="PPA ID received from SAS UUT as result of "
-                                     "PPA Creation is None")
-
-    logging.info('TriggerPpaCreation is in progress')
-
-    # Triggers most recent PPA Creation Status immediately and checks for the
-    # status of activity every 10 seconds until it is completed. If the status
-    # is not changed within 2 hours it will throw an exception.
-    signal.signal(signal.SIGALRM,
-                  lambda signum, frame:
-                  (_ for _ in ()).throw(
-                      Exception('Most Recent PPA Creation Status Check Timeout')))
-
-    # Timeout after 2 hours if it's not completed.
-    signal.alarm(7200)
-
-    # Check the Status of most recent ppa creation every 10 seconds.
-    while not self._sas_admin.GetPpaCreationStatus()['completed']:
-      time.sleep(10)
-
-    # Additional check to ensure whether PPA creation status has error.
-    self.assertFalse(self._sas_admin.GetPpaCreationStatus()['withError'],
-                     msg='There was an error while creating PPA')
-    signal.alarm(0)
-
-    return ppa_id
-
-  def assertPpaCreationFailure(self, ppa_creation_request):
-    """Trigger PPA Creation Admin API and asserts the failure response.
-
-    Triggers PPA creation to the SAS UUT and handles exception received if the
-    PPA creation returns error or times out.Checks the status of the PPA creation
-    by invoking the PPA creation status API.If the status is complete then looks for
-    withError set to True to declare ppa creation is failed. The status is checked
-    every 10 secs for up to 2 hours.
-
-    Args:
-      ppa_creation_request: A dictionary with a multiple key-value pair containing the
-        "cbsdIds", "palIds" and optional "providedContour"(a GeoJSON object).
-
-    Raises:
-      Exception for the following cases:
-         a. withError flag set to False even though most recent ppa creation
-         status is completed.
-         b. SAS UUT does not respond for longer time and timeout.
-         c. SAS UUT responds with success response rather than failure response.
-
-    """
-    response = self._sas_admin.TriggerPpaCreation(ppa_creation_request)
-    logging.info('TriggerPpaCreation is in progress')
-
-    # Triggers most recent PPA Creation Status immediately and checks for the status
-    # of activity every 10 seconds until it is completed. If the status is not changed
-    # within 2 hours it will throw an exception.
-    signal.signal(signal.SIGALRM,
-                  lambda signum, frame:
-                  (_ for _ in ()).throw(
-                      Exception('Most Recent PPA Creation Status Check Timeout')))
-
-    # Timeout after 2 hours if it's not completed.
-    signal.alarm(7200)
-
-    # Check the Status of most recent ppa creation every 10 seconds.
-    while not self._sas_admin.GetPpaCreationStatus()['completed']:
-      time.sleep(10)
-
-    # Expect the withError flag in status response should be toggled on to True
-    # to indicate PPA creation failure.
-    self.assertTrue(self._sas_admin.GetPpaCreationStatus()['withError'],
-                    msg='Expected:There is an error in create PPA. But '
-                        'PPA creation status indicates no error')
-    signal.alarm(0)
 
   def generate_WDB_1_default_config(self, filename):
     """Generates the WinnForum configuration for WDB.1"""
@@ -141,10 +46,8 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
       open(os.path.join('testcases', 'testdata', 'pal_record_1.json')))
 
     pal_record_b_0 = json.load(
-      open(os.path.join('testcases', 'testdata', 'pal_record_0.json')))
-    pal_record_b_1 = json.load(
-      open(os.path.join('testcases', 'testdata', 'pal_record_1.json')))
-
+      open(os.path.join('testcases', 'testdata', 'pal_record_2.json')))
+    
     # Set the PAL frequency.
     pal_low_frequency = 3570000000
     pal_high_frequency = 3580000000
@@ -153,13 +56,13 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     pal_records_a = makePalRecordsConsistent([pal_record_a_0, pal_record_a_1],
                                              pal_low_frequency, pal_high_frequency,
                                              device_a['userId'])
-    pal_records_b = makePalRecordsConsistent([pal_record_b_0, pal_record_b_1],
+    pal_records_b = makePalRecordsConsistent([pal_record_b_0],
                                              pal_low_frequency, pal_high_frequency,
                                              device_b['userId'])
 
     # Getting PAL IDs from pal records
     pal_ids_a = [pal_records_a[0]['palId'], pal_records_a[1]['palId']]
-    pal_ids_b = [pal_records_b[0]['palId'], pal_records_b[1]['palId']]
+    pal_ids_b = [pal_records_b[0]['palId']]
 
     # Creating conditionals for Cat B devices
     self.assertEqual(device_b['cbsdCategory'], 'B')
@@ -195,6 +98,9 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     # Load the configuration file
     config = loadConfig(config_filename)
 
+    # Very light checking of the config file.
+    self.assertEqual(len(config['registrationRequests']), len(config['palIds']))
+
     # Step 1: Register device(s) 'C' with conditional registration data
     cbsd_ids = self.assertRegistered(config['registrationRequests'],
                                      config['conditionalRegistrationData'])
@@ -202,9 +108,9 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     # Step 2: Request creation of PPA(s).
     # Forming of PPA creation request(s)
     ppa_creation_requests = []
-    for pal_ids in config['palIds']:
+    for cbsd_id, pal_ids in zip(cbsd_ids, config['palIds']):
       ppa_creation_requests.append({
-        "cbsdIds": cbsd_ids,
+        "cbsdIds": [cbsd_id],
         "palIds": pal_ids
       })
 
@@ -219,13 +125,9 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     self.TriggerDailyActivitiesImmediatelyAndWaitUntilComplete()
 
     # Step 5: Request creation of PPA(s) again
-    ppa_ids = []
     for ppa_creation_request in ppa_creation_requests:
       ppa_id = self.triggerPpaCreationAndWaitUntilComplete(ppa_creation_request)
       logging.debug('ppa_id received from SAS UUT:%s', ppa_id)
-      ppa_ids.append(ppa_id)
-
-    del ppa_ids
 
   def generate_WDB_2_default_config(self, filename):
     """Generates the WinnForum configuration for WDB.2"""
@@ -277,7 +179,7 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     config = loadConfig(config_filename)
 
     # Very light checking of the config file.
-    self.assertNotEqual(config['registrationRequests'], None)
+    self.assertGreater(len(config['registrationRequests']), 0)
 
     registration_requests = {
         'registrationRequest': config['registrationRequests']
@@ -289,7 +191,7 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
       self._sas_admin.InjectFccId({'fccId': registration_request['fccId']})
       self._sas_admin.InjectUserId({'userId': registration_request['userId']})
 
-    # Send registration request for CAT 'B' CBSD 'C' to SAS UUT.
+    # Send registration requests.
     registration_responses = self._sas.Registration(registration_requests)['registrationResponse']
 
     # Check registeration response,
@@ -313,5 +215,3 @@ class WinnfDatabaseUpdateTestcase(sas_testcase.SasTestCase):
     # responseCode should be 0 (SUCCESS).
     for registration_response in registration_responses:
       self.assertEqual(registration_response['response']['responseCode'], 0)
-
-    del registration_requests, registration_responses
