24bb81cc3d99dd3d42082fe9d965587396001704
Taran Furnell <20696359+TaranFurnell@users.noreply.github.com>
2018-04-30 12:39:06 -0700

    Correct database to correctly use https

--- src/harness/database.py
+++ src/harness/database.py
@@ -12,7 +12,28 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-"""A Generic HTTP Server implementation designed to serve a single file (for faking databases).
+"""A Generic HTTP Server implementation designed to serve arbitrary files.
+
+Note: This server is only intended for winnforum testing purposes, do not use
+this in a production environment.
+
+Example usage:
+d = DatabaseServer(
+    'Test',
+    'localhost',
+    8000,
+    cert_file=os.path.expanduser('certs/test_cert.crt'),
+    key_file=os.path.expanduser('certs/test_cert.key'),
+    ca_cert_file=os.path.expanduser('certs/test_cert_root.pem'),
+    authorization='testuser:password'
+    )
+d.setFileToServe('/allsitedata', 'allsitedata1')
+d.start()
+# DO SOME TESTING
+d.setFilesToServe({
+    '/allsitedata': 'allsitedata2',
+    '/othersitedata': 'othersitedata1'
+})
 """
 
 import base64
@@ -22,14 +43,6 @@ import ssl
 import threading
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-
-CIPHERS = [
-    'AES128-GCM-SHA256',              # TLS_RSA_WITH_AES_128_GCM_SHA256
-    'AES256-GCM-SHA384',              # TLS_RSA_WITH_AES_256_GCM_SHA384
-    'ECDHE-ECDSA-AES128-GCM-SHA256',  # TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-    'ECDHE-ECDSA-AES256-GCM-SHA384',  # TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-    'ECDHE-RSA-AES128-GCM-SHA256',    # TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
-]
 
 class DatabaseServer(threading.Thread):
   """A HTTP server which will serve a given file at the given file path."""
@@ -61,19 +74,17 @@ class DatabaseServer(threading.Thread):
         expected url).
     """
     super(DatabaseServer, self).__init__()
-    base_url = protocol + '://' + host_name + ':' + str(port) + '/'
     self.name = name
-    self.host_name = host_name
+    self.address = host_name + ':' + str(port)
     self.setDaemon(True)
-    self.server = DatabaseHTTPServer((host_name, port), DatabaseHandler, name, base_url, authorization)
+    self.server = DatabaseHTTPServer((host_name, port), DatabaseHandler, name, authorization)
     if protocol == 'https':
       self.server.socket = ssl.wrap_socket(
           self.server.socket,
           certfile=cert_file,
-          #keyfile=key_file,
+          keyfile=key_file,
           ca_certs=ca_cert_file,
           ssl_version=ssl.PROTOCOL_TLSv1_2,
-          ciphers=':'.join(CIPHERS),
           server_side=True)
 
   def run(self):
@@ -90,69 +101,61 @@ class DatabaseServer(threading.Thread):
     self.stopped = False
     while not self.stopped:
       logging.info('Started Database Server: %s at %s', self.name,
-                   self.host_name)
+                   self.address)
       try:
         self.server.serve_forever()
       except:
         traceback.print_exc()
         logging.error('HTTPServer %s at %s died on %s, restarting...',
-                      self.name, self.host_name,
+                      self.name, self.address,
                       datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
     logging.info('Shutting down Database Server:%s at %s', self.name,
-                 self.host_name)
+                 self.address)
 
   def setFileToServe(self, file_url, file_path):
-    """Add or update a single file url to path mapping.
+    """Remove existing path mappings and set a single file url to path mapping.
 
     Args:
       file_url: The url to provide the file on.
       file_path: The file path to provide on the given url.
     """
-    self.server.file_paths[file_url] = file_path
+    self.server.file_paths = {file_url: file_path}
 
   def setFilesToServe(self, file_url_file_path_dict):
-    """Add or update a multiple file url to path mappings.
+    """Remove existing path mappings and set multiple file url to path mappings.
 
     Args:
       file_url_file_path_dict: A dictionary of file_url: file_path as described
         in the setFileToServe description.
     """
-    self.server.file_paths.update(file_url_file_path_dict)
+    self.server.file_paths = file_url_file_path_dict
 
 class DatabaseHTTPServer(HTTPServer):
-  def __init__(self, server_address, RequestHandlerClass, name, base_url, authorization):
+  def __init__(self, server_address, RequestHandlerClass, name, authorization):
     HTTPServer.__init__(self, server_address, RequestHandlerClass)
     self.name = name
-    self.base_url = base_url
-    self.authorization = authorization
     self.file_paths = {}
+    # Encode the username and password for simple comparison. Expected form is:
+    #   'Basic Base64RepresentationOfUsername:Password'
+    self.authorization = 'Basic ' + base64.b64encode(authorization) if authorization else None
 
 class DatabaseHandler(SimpleHTTPRequestHandler):
   def translate_path(self, path):
     """Translate URL paths into filepaths."""
-    print 'request received'
-    print self.headers
-    if self.authorization and base64.b64decode(self.headers['Authorization']) != self.authorization:
-      # Return an error (404 atm because it's easy to do)
-      return ""
+    if self.server.authorization:
+      # Failed Authorization will return a 401 error and the reason why.
+      if 'Authorization' not in self.headers:
+        logging.info('Authorization Failed. No Auth header.')
+        self.send_response(401)
+        self.end_headers()
+        self.wfile.write('no auth header received.\n')
+        self.wfile.write('Ignore 404 response below.\n\n')
+        return ""
+      elif self.headers['Authorization'] != self.server.authorization:
+        logging.info('Authorization Failed. Incorrect username:password.')
+        self.send_response(401)
+        self.end_headers()
+        self.wfile.write('Incorrect username:password.\n')
+        self.wfile.write('Ignore 404 response below.\n\n')
+        return ""
     return self.server.file_paths[path] if path in self.server.file_paths else ""
-
-
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
-
-d = DatabaseServer(
-    'Test',
-    'localhost',
-    8000,
-    cert_file=os.path.expanduser('~/dev_certs/test_cert.pem'),
-    key_file=os.path.expanduser('~/dev_certs/test_cert.key'),
-    ca_cert_file=os.path.expanduser('~/dev_certs/test_cert_root.pem'),
-    authorization='Basic test:pass'
-    )
-d.setFileToServe('allsitedata', os.path.expanduser('~/allsitedata'))
-d.start()
-raw_input('Press enter to continue: ')
