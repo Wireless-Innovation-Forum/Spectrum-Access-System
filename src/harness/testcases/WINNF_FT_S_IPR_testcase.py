e986d4b067bd376d6ced5fb27832fad83807a033
Taran Furnell <20696359+TaranFurnell@users.noreply.github.com>
2018-05-04 10:37:16 -0700

    correct typo

--- src/harness/testcases/WINNF_FT_S_IPR_testcase.py
+++ src/harness/testcases/WINNF_FT_S_IPR_testcase.py
@@ -452,164 +452,6 @@ class FederalIncumbentProtectionTestcase(sas_testcase.SasTestCase):
         break
       time.sleep(config['pauseTime'])
 
-<<<<<<< HEAD
-  def generate_IPR_6_default_config(self, filename):
-    """Generates the WinnForum configuration for IPR_6"""
-
-    # Load Devices
-    device_a = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_a.json')))
-    device_a['installationParam']['latitude'] = 30.71570
-    device_a['installationParam']['longitude'] = -88.09350
-    device_b = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_b.json')))
-    device_b['installationParam']['latitude'] = 30.71571
-    device_b['installationParam']['longitude'] = -88.09351
-
-    # Pre-load conditionals and remove reg conditional fields from registration
-    # request.
-    conditional_keys = [
-        'cbsdCategory', 'fccId', 'cbsdSerialNumber', 'airInterface',
-        'installationParam', 'measCapability'
-    ]
-    reg_conditional_keys = [
-        'cbsdCategory', 'airInterface', 'installationParam', 'measCapability'
-    ]
-    conditionals_b = {key: device_b[key] for key in conditional_keys}
-    device_b = {
-        key: device_b[key]
-        for key in device_b
-        if key not in reg_conditional_keys
-    }
-
-    # Load grant requests.
-    grant_a = json.load(
-        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
-    grant_b = json.load(
-        open(os.path.join('testcases', 'testdata', 'grant_0.json')))
-
-    sas_test_harness_config = {
-        'sasTestHarnessName':
-            'SAS-Test-Harness-1',
-        'hostName':
-            'localhost',
-        'port':
-            9006,
-        'serverCert':
-            os.path.join('certs', 'server.cert'),
-        'serverKey':
-            os.path.join('certs', 'server.key'),
-        'caCert':
-            os.path.join('certs', 'ca.cert'),
-        'fullActivityDumpRecords': [
-            generateCbsdRecords([device_a], [[grant_a]])
-        ]
-    }
-
-    dpa_1 = {
-        'dpaId': 'east_dpa_4',
-        'points_builder': 'default (25, 10, 10, 10)',
-        'movelistMargin': 10
-    }
-
-    domain_proxy = {
-        'registrationRequests': [device_b],
-        'grantRequests': [grant_b],
-        'conditionalRegistrationData': [conditionals_b],
-        'cert': os.path.join('certs', 'domain_proxy.cert'),
-        'key': os.path.join('certs', 'domain_proxy.key')
-    }
-
-    config = {
-        'sasTestHarnessConfigs': [sas_test_harness_config],
-        'domainProxies': [domain_proxy],
-        'dpas': [dpa_1],
-    }
-    writeConfig(filename, config)
-
-  @configurable_testcase(generate_IPR_6_default_config)
-  def test_WINNF_FT_S_IPR_6(self, config_filename):
-    config = loadConfig(config_filename)
-    self.assertValidConfig(
-        config, {
-            'sasTestHarnessConfigs': list,
-            'domainProxies': list,
-            'dpas': list
-        })
-    # SAS UUT loads DPAs and is informed they are all inactive.
-    self._sas_admin.TriggerLoadDpas()
-    self._sas_admin.TriggerBulkDpaActivation({'activate': False})
-
-    # Steps 3 & 4 can be interleaved.
-    test_harnesses = []
-    for test_harness_config in config['sasTestHarnessConfigs']:
-      # Create test harness, notify the SAS UUT, and load FAD records.
-      test_harness = SasTestHarnessServer(
-          test_harness_config['sasTestHarnessName'],
-          test_harness_config['hostName'], test_harness_config['port'],
-          test_harness_config['serverCert'], test_harness_config['serverKey'],
-          test_harness_config['caCert'])
-      test_harness.start()
-      self._sas_admin.InjectPeerSas({
-          'certificateHash':
-              getCertificateFingerprint(test_harness_config['serverCert']),
-          'url':
-              test_harness.getBaseUrl()
-      })
-      test_harness.writeFadRecords(
-          test_harness_config['fullActivityDumpRecords'])
-      test_harnesses.append(test_harness)
-
-    # Register N2 CBSDs and request grants with SAS UUT from ND proxies.
-    domain_proxies = []
-    for domain_proxy_config in config['domainProxies']:
-      domain_proxy = DomainProxy(self,
-                                 ssl_cert=domain_proxy_config['cert'],
-                                 ssl_key=domain_proxy_config['key'])
-      domain_proxy.registerCbsdsAndRequestGrants(
-          domain_proxy_config['registrationRequests'],
-          domain_proxy_config['grantRequests'],
-          conditional_registration_data=domain_proxy_config['conditionalRegistrationData'])
-      domain_proxies.append(domain_proxy)
-
-    # Trigger, wait and download FAD records from SAS UUT and test harnesses.
-    sas_uut_fad = getFullActivityDumpSasUut(self._sas, self._sas_admin)
-    test_harness_fads = []
-    for test_harness in test_harnesses:
-      test_harness_fads.append(
-          getFullActivityDumpSasTestHarness(
-              test_harness.getSasTestHarnessInterface()))
-
-    # Trigger CPAS.
-    self.TriggerDailyActivitiesImmediatelyAndWaitUntilComplete()
-
-    # Calculate DPA movelists for each DPA.
-    all_dpas = []
-    for dpa_config in config['dpas']:
-      dpa = dpa_mgr.BuildDpa(dpa_config['dpaId'], dpa_config['points_builder'])
-      dpa.SetGrantsFromFad(sas_uut_fad, test_harness_fads)
-      dpa.ComputeMoveLists()
-      all_dpas.append(dpa)
-
-    # Connectivity between ESCs and SAS UUT is broken.
-    self._sas_admin.TriggerEscDisconnect()
-
-    # Wait till all heartbeats must suspend grants that are on DPA movelists.
-    time.sleep(240)
-
-    # Heartbeat with all domain proxies.
-    for domain_proxy in domain_proxies:
-      domain_proxy.heartbeatForAllActiveGrants()
-    grant_info = data.getAuthorizedGrantsFromDomainProxies(domain_proxies)
-    # Check that for each DPA the SAS UUT has not exceeded the allowed
-    # interference threshold.
-    for dpa, dpa_config in zip(all_dpas, config['dpas']):
-      self.assertTrue(dpa.CheckInterference(
-          sas_uut_active_grants=grant_info,
-          margin_db=dpa_config['movelistMargin']))
-
-=======
->>>>>>> Corrected domain proxy usage in IPR.5
   def generate_IPR_3_default_config(self, filename):
     """Generates the WinnForum configuration for IPR_3"""
 
@@ -698,7 +540,7 @@ class FederalIncumbentProtectionTestcase(sas_testcase.SasTestCase):
     # Load grant request.
     grant_b = json.load(
         open(os.path.join('testcases', 'testdata', 'grant_0.json')))
-    frequency_range = grant_a['operationParam']['operationFrequencyRange']
+    frequency_range = grant_b['operationParam']['operationFrequencyRange']
     dpa_1 = {
         'dpaId': 'Pascagoula',
         'frequencyRange': frequency_range
@@ -901,7 +743,7 @@ class FederalIncumbentProtectionTestcase(sas_testcase.SasTestCase):
         'hostName':
             'localhost',
         'port':
-            9001,
+            9006,
         'serverCert':
             os.path.join('certs', 'server.cert'),
         'serverKey':
