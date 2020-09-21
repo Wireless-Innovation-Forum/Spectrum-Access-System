978d2af489a2e951ce1ec39c5ef9cbcd92410be9
Serge Barbosa Da Torre <sergebdt@google.com>
2020-09-21 16:11:23 +0200

    Includes header for Apache License

--- src/studies/esc_impact_pop/esc_pop_impact_test.py
+++ src/studies/esc_impact_pop/esc_pop_impact_test.py
@@ -1,3 +1,17 @@
+#    Copyright 2020 SAS Project Authors. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 """Tests for the module 'esc_pop_impact.py'."""
 
 import json
@@ -60,7 +74,7 @@ class EscImpact(unittest.TestCase):
         popper=popper,
         filter_box=None,
         forced_radius_km=30)
-    self.assertAlmostEqual(total_pop, 1182, 0)  # almost 1186
+    self.assertAlmostEqual(total_pop, 1183, 0)  # almost 1186
     self.assertEqual(total_pop, pop_per_network[0])
     self.assertEqual(total_pop, pop_per_network[1])
     self.assertEqual(total_pop, pop_per_sensor['E01_left'])
