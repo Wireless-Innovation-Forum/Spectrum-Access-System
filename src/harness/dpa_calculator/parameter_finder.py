e027bef8d7cafe70719aa762fa49fa16a0f66cc5
nikojpapa <npapa1994@gmail.com>
2021-11-15 16:17:29 -0700

    binary search refactor

--- src/harness/dpa_calculator/parameter_finder.py
+++ src/harness/dpa_calculator/parameter_finder.py
@@ -1,43 +1,53 @@
-from math import inf
 from statistics import mean
-from typing import Callable
+from typing import Callable, Optional
 
 
 class ParameterFinder:
     def __init__(self, function: Callable[[int], float], target: int, max_parameter: int = 500):
         self._function = function
         self._target = target
-        self._min = 0
-        self._max = 1
         self._max_parameter = max_parameter
 
+        self._min = 0
+        self._max = 1
+
     def find(self) -> int:
-        result = inf
-        in_phase_one = True
-        phase_one_counter = 0
-        while True:
-            if self._target < self._function(self._max_parameter):
-                return self._max_parameter
-            if self._target > self._function(self._min):
-                return self._min
-            if self._min > self._max:
-                return self._max
-            if result == self._target:
-                return self._current_parameter
+        if self._results_extremity is not None:
+            return self._results_extremity
+        self._grow_max_input()
+        return self._perform_binary_search()
 
-            if self._target > result:
-                in_phase_one = False
+    @property
+    def _results_extremity(self) -> Optional[int]:
+        if self._target < self._function(self._max_parameter):
+            return self._max_parameter
+        if self._target > self._function(self._min):
+            return self._min
 
-            if in_phase_one:
-                self._min = self._current_parameter
-                self._max = self._min + 2 ** phase_one_counter
-                phase_one_counter += 1
-            else:
-                if self._target < result:
-                    self._min = self._current_parameter + 1
-                elif self._target > result:
-                    self._max = self._current_parameter - 1
-            result = self._function(self._current_parameter)
+    def _grow_max_input(self) -> None:
+        phase_one_counter = -1
+        while self._target < self._function_result_with_current_parameter:
+            phase_one_counter += 1
+            self._min = self._current_parameter
+            self._max = self._min + 2 ** phase_one_counter
+
+    def _perform_binary_search(self) -> int:
+        while self._input_found() is None:
+            if self._target < self._function_result_with_current_parameter:
+                self._min = self._current_parameter + 1
+            elif self._target > self._function_result_with_current_parameter:
+                self._max = self._current_parameter - 1
+        return self._input_found()
+
+    def _input_found(self) -> int:
+        if self._min > self._max:
+            return self._min
+        if self._function_result_with_current_parameter == self._target:
+            return self._current_parameter
+
+    @property
+    def _function_result_with_current_parameter(self) -> float:
+        return self._function(self._current_parameter)
 
     @property
     def _current_parameter(self) -> int:
